dd9c601e9e93f1ad56c4e2f364f8318ba5c099f6
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-14 11:04:48 +0200

    Task 9 and .sln file uploaded.
    Two solutions of task 9.

---
+++ 01.FirstStepsInCoding_Lab/09.YardGreening.v2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _09.YardGreening.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal sqMeters = decimal.Parse(Console.ReadLine());
+            decimal pricePerSqMeter = 7.61m;
+            decimal discountMultiplyer = 0.82m;
+            decimal totalSum = pricePerSqMeter * sqMeters;
+            decimal sumToPay = totalSum * discountMultiplyer;
+            decimal savedMoney = totalSum - sumToPay;
+
+            Console.WriteLine($"The final price is: {sumToPay:f2} lv.");
+            Console.WriteLine($"The discount is: {savedMoney:f2} lv.");
+        }
+    }
+}
---
+++ 01.FirstStepsInCoding_Lab/09.YardGreening/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _09.YardGreening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pricePerSqMeter = 7.61;
+            double discount = 0.18;
+
+            double sqMetersToBeGreened = double.Parse(Console.ReadLine());
+
+            double totalCost = sqMetersToBeGreened * pricePerSqMeter;
+            double discountCost = totalCost * discount;
+            double payCost = totalCost - discountCost;
+
+            Console.WriteLine($"The final price is: {payCost} lv.");
+            Console.WriteLine($"The discount is: {discountCost} lv.");
+        }
+    }
+}
