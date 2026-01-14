6af7f0dc358de90d0543d2a2f4153b9aa5e0c1cf
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-14 11:02:57 +0200

    Task 8 uploaded.
    Two solutions.

---
+++ 01.FirstStepsInCoding_Lab/08.PetShop.v2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _08.PetShop.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogFoodCount = int.Parse(Console.ReadLine());
+            int catFoodCount = int.Parse(Console.ReadLine());
+
+            double dogFoodPrice = 2.5;
+            double catFoodPrice = 4.0;
+
+            double finalSum = dogFoodCount * dogFoodPrice + catFoodCount * catFoodPrice;
+            Console.WriteLine($"{finalSum} lv.");
+        }
+    }
+}
---
+++ 01.FirstStepsInCoding_Lab/08.PetShop/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _08.PetShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dogPouch = 2.50;
+            double catPouch = 4.00;
+
+            int dogPouchCount = int.Parse(Console.ReadLine());
+            int catPouchCount = int.Parse(Console.ReadLine());
+
+            double dogFoodPrice = dogPouch * dogPouchCount;
+            double catFoodPrice = catPouch * catPouchCount;
+            double finalSum = dogFoodPrice + catFoodPrice;
+
+            Console.WriteLine(finalSum + " lv.");
+        }
+    }
+}
