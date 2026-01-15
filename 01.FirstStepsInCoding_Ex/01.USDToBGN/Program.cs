314566a76dd78ed7a7c4e1545e7b978ab08b2a04
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-15 10:28:32 +0200

    Tasks 1 and 2 uploaded.

---
+++ 01.FirstStepsInCoding_Ex/01.USDToBGN/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _01.USDToBGN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double usdInput = double.Parse(Console.ReadLine());
+            double bgnPerUsd = 1.79549;
+            double bgnOutput = usdInput * bgnPerUsd;
+            Console.WriteLine(bgnOutput);
+        }
+    }
+}
---
+++ 01.FirstStepsInCoding_Ex/02.RadiansToDegrees/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _02.RadiansToDegrees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double inputRadians = double.Parse(Console.ReadLine());
+            double angelInDegrees = inputRadians * 180 / Math.PI;
+            Console.WriteLine(angelInDegrees);
+        }
+    }
+}
