e8dbee8847f7e42ed1a0e739d87e5a4bd9b39ec4
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-15 10:31:12 +0200

    Task 5 uploaded.

---
+++ 01.FirstStepsInCoding_Ex/05.SuppliesForSchool/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _05.SuppliesForSchool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int penPackages = int.Parse(Console.ReadLine());
+            int markerPackages = int.Parse(Console.ReadLine());
+            int litersCleaner = int.Parse(Console.ReadLine());
+            int inputPercentDiscount = int.Parse(Console.ReadLine());
+
+            double penPackagePrice = 5.80;
+            double markerPackagePrice = 7.20;
+            double cleanerPricePerLiter = 1.20;
+
+            double pensCost = penPackagePrice * penPackages;
+            double markersCost = markerPackagePrice * markerPackages;
+            double cleanerCost = cleanerPricePerLiter * litersCleaner;
+            double totalSum = pensCost + markersCost + cleanerCost;
+            double sumToPay = totalSum - totalSum * inputPercentDiscount / 100.0;
+
+            Console.WriteLine(sumToPay);
+        }
+    }
+}
