d59b4afe27bce7359a6b4880079aeef9487cb3f3
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-16 14:00:30 +0200

    Task 8 uploaded.

---
+++ 01.FirstStepsInCoding_Ex/08.BasketballEquipment/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _08.BasketballEquipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int annualFee = int.Parse(Console.ReadLine());
+
+            double sneakersMultiplyer = 0.4;
+            double jerseyMultiplyer = 0.2;
+            double ballPercentage = 0.25;
+            double accessoriesPercentage = 0.2;
+
+            double sneakersPrice = annualFee * (1 - sneakersMultiplyer);
+            double jerseyPrice = sneakersPrice * (1 - jerseyMultiplyer);
+            double ballPrice = jerseyPrice * ballPercentage;
+            double accessoriesPrice = ballPrice * accessoriesPercentage;
+
+            double annualCosts = annualFee + sneakersPrice + jerseyPrice + ballPrice + accessoriesPrice;
+            Console.WriteLine(annualCosts);
+        }
+    }
+}
