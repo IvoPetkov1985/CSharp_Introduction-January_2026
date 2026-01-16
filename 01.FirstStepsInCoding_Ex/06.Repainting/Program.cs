c2e08f9bf9ad7350abc0f500cd77f51fcc54caa3
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-16 13:59:34 +0200

    Task 6 uploaded.

---
+++ 01.FirstStepsInCoding_Ex/06.Repainting/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _06.Repainting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nylonPricePerSqM = 1.50;
+            double paintPricePerL = 14.50;
+            double thinnerPricePerL = 5.00;
+
+            double workMultiplyer = 0.3;
+            double paintMultiplyer = 1.10;
+            int additionalNylonInSqM = 2;
+            double bagsPrice = 0.40;
+
+            int nylonInSqM = int.Parse(Console.ReadLine());
+            int paintInL = int.Parse(Console.ReadLine());
+            int thinnerInL = int.Parse(Console.ReadLine());
+            int hoursWork = int.Parse(Console.ReadLine());
+
+            double nylonCost = (nylonInSqM + additionalNylonInSqM) * nylonPricePerSqM;
+            double paintCost = paintInL * paintMultiplyer * paintPricePerL;
+            double thinnerCost = thinnerInL * thinnerPricePerL;
+            double materialsCost = nylonCost + paintCost + thinnerCost + bagsPrice;
+            double totalCost = materialsCost + materialsCost * workMultiplyer * hoursWork;
+
+            Console.WriteLine(totalCost);
+        }
+    }
+}
