6191922a0724f916571424bec8ac7d42db53bfcc
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-16 14:01:04 +0200

    Task 9 uploaded.

---
+++ 01.FirstStepsInCoding_Ex/09.FishTank/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _09.FishTank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthInCm = int.Parse(Console.ReadLine());
+            int widthInCm = int.Parse(Console.ReadLine());
+            int heightInCm = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            int volumeInCm = lengthInCm * widthInCm * heightInCm;
+            double volumeInDm = volumeInCm * 0.001;
+            double freeVolume = volumeInDm - (volumeInDm * percent / 100);
+            Console.WriteLine(freeVolume);
+        }
+    }
+}
