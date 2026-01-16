b64e216b542c1e25197d25433374df9b0e5de512
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-16 14:00:03 +0200

    Task 7 uploaded.

---
+++ 01.FirstStepsInCoding_Ex/07.FoodDelivery/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _07.FoodDelivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double chickenMenuPrice = 10.35;
+            double fishMenuPrice = 12.40;
+            double veggieMenuPrice = 8.15;
+            double deliveryPrice = 2.50;
+            double dessertMultiplyer = 0.2;
+
+            int chickenMenus = int.Parse(Console.ReadLine());
+            int fishMenus = int.Parse(Console.ReadLine());
+            int veggieMenus = int.Parse(Console.ReadLine());
+
+            double menusPrice = chickenMenus * chickenMenuPrice +
+                fishMenus * fishMenuPrice +
+                veggieMenus * veggieMenuPrice;
+
+            double dessertPrice = menusPrice * dessertMultiplyer;
+            double totalPurchasePrice = menusPrice + dessertPrice + deliveryPrice;
+
+            Console.WriteLine(totalPurchasePrice);
+        }
+    }
+}
