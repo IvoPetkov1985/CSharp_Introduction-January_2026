4694f15ee2254b5ebdd2a67b7582788f138e1d3a
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-18 09:58:52 +0200

    Task 5 uploaded.
    Two solutions.

---
+++ 01.FirstStepsInCoding_MoreEx/05.TrainingLab.v2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _05.TrainingLab.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double roomLength = double.Parse(Console.ReadLine());
+            double roomWidth = double.Parse(Console.ReadLine());
+
+            double roomLengthInCm = roomLength * 100;
+            double roomWidthInCm = roomWidth * 100;
+
+            int pathWidth = 100;
+            int workplaceWidth = 70;
+            int workplaceLength = 120;
+            int missingWorkplaces = 3;
+
+            int workplacesInRow = (int)(roomWidthInCm - pathWidth) / workplaceWidth;
+            int workplacesInCol = (int)roomLengthInCm / workplaceLength;
+            int totalWorkplaces = workplacesInRow * workplacesInCol - missingWorkplaces;
+            Console.WriteLine(totalWorkplaces);
+        }
+    }
+}
---
+++ 01.FirstStepsInCoding_MoreEx/05.TrainingLab/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _05.TrainingLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double roomLengthInM = double.Parse(Console.ReadLine());
+            double roomWidthInM = double.Parse(Console.ReadLine());
+
+            double lengthInCm = roomLengthInM * 100;
+            double widthInCm = roomWidthInM * 100;
+            int missingPlaces = 3;
+            int placeWidth = 70;
+            int placeLength = 120;
+
+            double widthWithoutPath = widthInCm - 100;
+            double placesInRow = Math.Truncate(widthWithoutPath / placeWidth);
+            double placesInCol = Math.Truncate(lengthInCm / placeLength);
+            double totalPlaces = placesInCol * placesInRow - missingPlaces;
+            Console.WriteLine(totalPlaces);
+        }
+    }
+}
