294b28786ccf78a0ee5d2c474088fb2960b30157
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-18 09:56:54 +0200

    Tasks 1 and 2 uploaded.

---
+++ 01.FirstStepsInCoding_MoreEx/01.TrapeziodArea/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.TrapeziodArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseB1 = double.Parse(Console.ReadLine());
+            double baseB2 = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double trapezoidArea = (baseB1 + baseB2) * height / 2;
+
+            Console.WriteLine($"{trapezoidArea:F2}");
+        }
+    }
+}
---
+++ 01.FirstStepsInCoding_MoreEx/02.TriangleArea/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _02.TriangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sideSize = double.Parse(Console.ReadLine());
+            double heightSize = double.Parse(Console.ReadLine());
+
+            double triangleArea = sideSize * heightSize / 2;
+
+            Console.WriteLine($"{triangleArea:F2}");
+        }
+    }
+}
