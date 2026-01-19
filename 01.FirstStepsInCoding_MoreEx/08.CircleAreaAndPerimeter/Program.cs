29fd5211e303400e064a2e16b43d511a2d2c2c45
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-19 13:59:22 +0200

    Task 8 uploaded.

---
+++ 01.FirstStepsInCoding_MoreEx/08.CircleAreaAndPerimeter/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _08.CircleAreaAndPerimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = double.Parse(Console.ReadLine());
+
+            double calculatedArea = Math.PI * Math.Pow(radius, 2);
+            double calculatedPerimeter = 2 * Math.PI * radius;
+
+            Console.WriteLine($"{calculatedArea:F2}");
+            Console.WriteLine($"{calculatedPerimeter:F2}");
+        }
+    }
+}
