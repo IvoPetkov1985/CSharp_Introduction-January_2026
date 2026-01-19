ca4cabaa7ce2f0bd8bf93cdf59922d41a3a84ce5
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-19 14:00:10 +0200

    Task 9 uploaded.
    If-else construction used.

---
+++ 01.FirstStepsInCoding_MoreEx/09.WeatherForecast/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _09.WeatherForecast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = string.Empty;
+
+            if (input == "sunny")
+            {
+                output = "It's warm outside!";
+            }
+            else
+            {
+                output = "It's cold outside!";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
