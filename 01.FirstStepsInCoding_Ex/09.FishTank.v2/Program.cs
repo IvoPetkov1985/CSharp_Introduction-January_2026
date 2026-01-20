31b728ce81014df893da3326c07b721884235103
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-20 10:30:26 +0200

    Task 9 uploaded.
    Another solution added.

---
+++ 01.FirstStepsInCoding_Ex/09.FishTank.v2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _09.FishTank.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthInCm = int.Parse(Console.ReadLine());
+            int widthInCm = int.Parse(Console.ReadLine());
+            int heightInCm = int.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine());
+
+            int fullVolume = lengthInCm * widthInCm * heightInCm;
+            double litersWater = (fullVolume - fullVolume * percentage / 100) / 1000;
+
+            Console.WriteLine(litersWater);
+        }
+    }
+}
