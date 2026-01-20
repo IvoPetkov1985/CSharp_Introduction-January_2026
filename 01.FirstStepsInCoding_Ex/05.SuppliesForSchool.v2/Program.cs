be491477efd9453688cad62bf0f6e0e0d4e825a9
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-20 10:28:01 +0200

    Tasks 5 and 6 uploaded.
    Other solutions.

---
+++ 01.FirstStepsInCoding_Ex/05.SuppliesForSchool.v2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _05.SuppliesForSchool.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pensPackagePrice = 5.80;
+            double markersPackagePrice = 7.20;
+            double cleanerLiterPrice = 1.20;
+
+            int pensPackagesCount = int.Parse(Console.ReadLine());
+            int markersPackagesCount = int.Parse(Console.ReadLine());
+            int litersCleaner = int.Parse(Console.ReadLine());
+            int percentageDiscount = int.Parse(Console.ReadLine());
+
+            double totalSum = (pensPackagesCount * pensPackagePrice +
+                markersPackagesCount * markersPackagePrice +
+                litersCleaner * cleanerLiterPrice) *
+                (1 - percentageDiscount / 100.0);
+            Console.WriteLine(totalSum);
+        }
+    }
+}
---
+++ 01.FirstStepsInCoding_Ex/06.Repainting.v2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _06.Repainting.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double safetyNylonSqMPrice = 1.50;
+            double paintPerLiterPrice = 14.50;
+            double thinnerPerLiterPrice = 5.00;
+
+            double addedPaintMultiplyer = 1.10;
+            int addedSafetyNylonSqM = 2;
+            double nylonBagsPrice = 0.40;
+            double workPerHourMultiplyer = 0.30;
+
+            int neededSafetyNylon = int.Parse(Console.ReadLine());
+            int neededPaint = int.Parse(Console.ReadLine());
+            int neededThinner = int.Parse(Console.ReadLine());
+            int hoursWork = int.Parse(Console.ReadLine());
+
+            double sumForMaterials = (neededSafetyNylon + addedSafetyNylonSqM) * safetyNylonSqMPrice +
+                neededPaint * addedPaintMultiplyer * paintPerLiterPrice +
+                neededThinner * thinnerPerLiterPrice +
+                nylonBagsPrice;
+
+            double sumForWork = sumForMaterials * workPerHourMultiplyer * hoursWork;
+
+            Console.WriteLine(sumForMaterials + sumForWork);
+        }
+    }
+}
