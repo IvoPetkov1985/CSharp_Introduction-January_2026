b492d62429c71bd100cca477e153769886873ca6
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-20 10:29:30 +0200

    Tasks 7 and 8 uploaded.

---
+++ 01.FirstStepsInCoding_Ex/07.FoodDelivery.v2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _07.FoodDelivery.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double chickenMenuPrice = 10.35;
+            double fishMenuPrice = 12.40;
+            double veggieMenuPrice = 8.15;
+            int dessertPercent = 20;
+            double deliveryPrice = 2.50;
+
+            int chickenMenus = int.Parse(Console.ReadLine());
+            int fishMenus = int.Parse(Console.ReadLine());
+            int veggieMenus = int.Parse(Console.ReadLine());
+
+            double totalSum = (chickenMenus * chickenMenuPrice + fishMenus * fishMenuPrice + veggieMenus * veggieMenuPrice) *
+                (1 + dessertPercent / 100.0) + deliveryPrice;
+            Console.WriteLine(totalSum);
+        }
+    }
+}
---
+++ 01.FirstStepsInCoding_Ex/08.BasketballEquipment.v2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _08.BasketballEquipment.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sneakersPercent = 40;
+            int outfitPercent = 20;
+            int ballDivisor = 4;
+            int accessoriesDivisor = 5;
+
+            int tax = int.Parse(Console.ReadLine());
+
+            double sneakersPrice = tax * (1 - sneakersPercent / 100.0);
+            double outfitPrice = sneakersPrice * (1 - outfitPercent / 100.0);
+            double ballPrice = outfitPrice / ballDivisor;
+            double accessoriesPrice = ballPrice / accessoriesDivisor;
+
+            double totalSum = tax + sneakersPrice + outfitPrice + ballPrice + accessoriesPrice;
+
+            Console.WriteLine(totalSum);
+        }
+    }
+}
