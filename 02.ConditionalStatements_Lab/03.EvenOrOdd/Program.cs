c79cf10d5ab0bc4493c659fc8b098d731ef6b763
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-21 11:56:32 +0200

    Tasks 3 and 4 uploaded.

---
+++ 02.ConditionalStatements_Lab/03.EvenOrOdd/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _03.EvenOrOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            string output = string.Empty;
+
+            if (inputNumber % 2 == 0)
+            {
+                output = "even";
+            }
+            else
+            {
+                output = "odd";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ 02.ConditionalStatements_Lab/04.PasswordGuess/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _04.PasswordGuess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPassword = Console.ReadLine();
+            string searchedPassword = "s3cr3t!P@ssw0rd";
+            string output = string.Empty;
+
+            if (inputPassword == searchedPassword)
+            {
+                output = "Welcome";
+            }
+            else
+            {
+                output = "Wrong password!";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
