688478e5116e04da18596460722aed9626888242
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-21 11:57:05 +0200

    Tasks 5 and 6 uploaded.

---
+++ 02.ConditionalStatements_Lab/05.NumberFrom100To200/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _05.NumberFrom100To200
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            int lowLimit = 100;
+            int highLimit = 200;
+            string output = string.Empty;
+
+            if (inputNumber < lowLimit)
+            {
+                output = "Less than 100";
+            }
+            else if (inputNumber <= highLimit)
+            {
+                output = "Between 100 and 200";
+            }
+            else if (inputNumber > highLimit)
+            {
+                output = "Greater than 200";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ 02.ConditionalStatements_Lab/06.SpeedInfo/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _06.SpeedInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double speed = double.Parse(Console.ReadLine());
+            string info = string.Empty;
+
+            if (speed <= 10)
+            {
+                info = "slow";
+            }
+            else if (speed <= 50)
+            {
+                info = "average";
+            }
+            else if (speed <= 150)
+            {
+                info = "fast";
+            }
+            else if (speed <= 1000)
+            {
+                info = "ultra fast";
+            }
+            else if (speed > 1000)
+            {
+                info = "extremely fast";
+            }
+
+            Console.WriteLine(info);
+        }
+    }
+}
