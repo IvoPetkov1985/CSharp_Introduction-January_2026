a5d4da15b552539aa7204a33c1a58a415b3c049c
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-21 11:57:57 +0200

    Task 7 and .sln file uploaded.
    Two solutions of task 7.

---
+++ 02.ConditionalStatements_Lab/07.AreaOfFigures.v2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _07.AreaOfFigures.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            double area = 0;
+
+            if (figure == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                area = Math.Pow(side, 2);
+            }
+            else if (figure == "rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                area = sideA * sideB;
+            }
+            else if (figure == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                area = Math.PI * Math.Pow(radius, 2);
+            }
+            else if (figure == "triangle")
+            {
+                double triangleSide = double.Parse(Console.ReadLine());
+                double triangleHeight = double.Parse(Console.ReadLine());
+                area = triangleSide * triangleHeight / 2;
+            }
+
+            Console.WriteLine($"{area:F3}");
+        }
+    }
+}
---
+++ 02.ConditionalStatements_Lab/07.AreaOfFigures/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _07.AreaOfFigures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string figureType = Console.ReadLine();
+            double lengthOfSideOrRadius = double.Parse(Console.ReadLine());
+            double result = 0;
+
+            switch (figureType)
+            {
+                case "square":
+                    result = Math.Pow(lengthOfSideOrRadius, 2);
+                    break;
+                case "rectangle":
+                    double secondSide = double.Parse(Console.ReadLine());
+                    result = lengthOfSideOrRadius * secondSide;
+                    break;
+                case "circle":
+                    result = Math.PI * Math.Pow(lengthOfSideOrRadius, 2);
+                    break;
+                case "triangle":
+                    double height = double.Parse(Console.ReadLine());
+                    result = lengthOfSideOrRadius * height / 2;
+                    break;
+            }
+
+            Console.WriteLine("{0:F3}", result);
+        }
+    }
+}
