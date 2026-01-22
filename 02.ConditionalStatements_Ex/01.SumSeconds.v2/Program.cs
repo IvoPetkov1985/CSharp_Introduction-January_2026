4c40a538b9ab33b1c8081027f8d27833242ebfcf
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-22 15:40:27 +0200

    Task 1 uploaded.
    Two solutions.

---
+++ 02.ConditionalStatements_Ex/01.SumSeconds.v2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _01.SumSeconds.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secondTime = int.Parse(Console.ReadLine());
+            int thirdTime = int.Parse(Console.ReadLine());
+
+            int totalSeconds = firstTime + secondTime + thirdTime;
+            int minutes = totalSeconds / 60;
+            int seconds = totalSeconds % 60;
+
+            Console.WriteLine($"{minutes}:{seconds:D2}");
+        }
+    }
+}
---
+++ 02.ConditionalStatements_Ex/01.SumSeconds/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01.SumSeconds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTimeInS = int.Parse(Console.ReadLine());
+            int secondTimeInS = int.Parse(Console.ReadLine());
+            int thirdTimeInS = int.Parse(Console.ReadLine());
+
+            int totalTimeInS = firstTimeInS + secondTimeInS + thirdTimeInS;
+            int minutes = totalTimeInS / 60;
+            int seconds = totalTimeInS % 60;
+
+            if (seconds < 10)
+            {
+                Console.WriteLine($"{minutes}:0{seconds}");
+            }
+            else
+            {
+                Console.WriteLine($"{minutes}:{seconds}");
+            }
+        }
+    }
+}
