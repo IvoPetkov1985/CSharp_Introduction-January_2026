624bae08224f6e49c73a4acffcecf0428ee9b7a6
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-22 15:42:44 +0200

    Task 4 and .sln file uploaded.

---
+++ 02.ConditionalStatements_Ex/04.ToyShop/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _04.ToyShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double puzzlePrice = 2.60;
+            double dollPrice = 3.00;
+            double hugBearPrice = 4.10;
+            double minionPrice = 8.20;
+            double truckPrice = 2.00;
+            int discountPercent = 25;
+            int rentPercent = 10;
+
+            double travelPrice = double.Parse(Console.ReadLine());
+            int puzzles = int.Parse(Console.ReadLine());
+            int dolls = int.Parse(Console.ReadLine());
+            int hugBears = int.Parse(Console.ReadLine());
+            int minions = int.Parse(Console.ReadLine());
+            int trucks = int.Parse(Console.ReadLine());
+
+            int toysCount = puzzles + dolls + hugBears + minions + trucks;
+            double totalSum = puzzles * puzzlePrice + dolls * dollPrice + hugBears * hugBearPrice + minions * minionPrice + trucks * truckPrice;
+
+            if (toysCount >= 50)
+            {
+                totalSum *= 1 - discountPercent / 100.0;
+            }
+
+            totalSum *= 1 - rentPercent / 100.0;
+
+            if (totalSum >= travelPrice)
+            {
+                Console.WriteLine($"Yes! {totalSum - travelPrice:F2} lv left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! {travelPrice - totalSum:F2} lv needed.");
+            }
+        }
+    }
+}
