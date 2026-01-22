26a65a5701e88057810b52684c8af7f5a9a4582e
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-22 15:41:19 +0200

    Task 2 uploaded.

---
+++ 02.ConditionalStatements_Ex/02.BonusScore/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _02.BonusScore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialPoints = int.Parse(Console.ReadLine());
+            double bonusPoints = 0;
+
+            if (initialPoints <= 100)
+            {
+                bonusPoints = 5;
+            }
+            else if (initialPoints <= 1000)
+            {
+                bonusPoints = initialPoints * 0.20;
+            }
+            else
+            {
+                bonusPoints = initialPoints * 0.10;
+            }
+
+            if (initialPoints % 2 == 0)
+            {
+                bonusPoints++;
+            }
+            else if (initialPoints % 10 == 5)
+            {
+                bonusPoints += 2;
+            }
+
+            double totalPoints = initialPoints + bonusPoints;
+            Console.WriteLine(bonusPoints);
+            Console.WriteLine(totalPoints);
+        }
+    }
+}
