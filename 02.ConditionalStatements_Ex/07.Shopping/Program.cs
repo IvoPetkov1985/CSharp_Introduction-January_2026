85ea14d2c782872c6c30b718f5ea8a597a449bb5
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-23 11:48:41 +0200

    Tasks 7 and 8 uploaded.

---
+++ 02.ConditionalStatements_Ex/07.Shopping/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _07.Shopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double videoCardPrice = 250.00;
+            int processorPercent = 35;
+            int ramStickPercent = 10;
+            int discountPercent = 15;
+
+            double budget = double.Parse(Console.ReadLine());
+            int videoCardsCount = int.Parse(Console.ReadLine());
+            int processorsCount = int.Parse(Console.ReadLine());
+            int ramSticksCount = int.Parse(Console.ReadLine());
+
+            double processorPrice = videoCardPrice * videoCardsCount * processorPercent / 100.0;
+            double ramStickPrice = videoCardPrice * videoCardsCount * ramStickPercent / 100.0;
+
+            double sum = videoCardsCount * videoCardPrice + processorsCount * processorPrice + ramSticksCount * ramStickPrice;
+
+            if (videoCardsCount > processorsCount)
+            {
+                sum *= 1 - discountPercent / 100.0;
+            }
+
+            double moneyDiff = Math.Abs(budget - sum);
+
+            if (budget >= sum)
+            {
+                Console.WriteLine($"You have {moneyDiff:F2} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {moneyDiff:F2} leva more!");
+            }
+        }
+    }
+}
---
+++ 02.ConditionalStatements_Ex/08.LunchBreak/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _08.LunchBreak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lunchDivisor = 8;
+            int restDivisor = 4;
+
+            string seriesName = Console.ReadLine();
+            int seriesDuration = int.Parse(Console.ReadLine());
+            int lunchBreakDuration = int.Parse(Console.ReadLine());
+
+            double timeForLunch = lunchBreakDuration / (lunchDivisor * 1.0);
+            double timeForRest = lunchBreakDuration / (restDivisor * 1.0);
+            double timeForWatching = lunchBreakDuration - timeForLunch - timeForRest;
+
+            if (timeForWatching >= seriesDuration)
+            {
+                Console.WriteLine($"You have enough time to watch {seriesName} and left with {Math.Ceiling(timeForWatching - seriesDuration)} minutes free time.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough time to watch {seriesName}, you need {Math.Ceiling(seriesDuration - timeForWatching)} more minutes.");
+            }
+        }
+    }
+}
