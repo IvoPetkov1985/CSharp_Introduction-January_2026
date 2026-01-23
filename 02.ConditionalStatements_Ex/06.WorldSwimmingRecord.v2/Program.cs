cac7134004e134b45509883d28ea7b86cb4efb4f
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-23 11:47:54 +0200

    Task 6 uploaded.
    Two solutions.

---
+++ 02.ConditionalStatements_Ex/06.WorldSwimmingRecord.v2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _06.WorldSwimmingRecord.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int delayInM = 15;
+            double delayTime = 12.5;
+
+            double worldRecord = double.Parse(Console.ReadLine());
+            double distanceInM = double.Parse(Console.ReadLine());
+            double timePerM = double.Parse(Console.ReadLine());
+
+            double timeAllDistance = distanceInM * timePerM;
+            double delays = Math.Floor(distanceInM / delayInM);
+            double addedTime = delays * delayTime;
+            double totalTime = timeAllDistance + addedTime;
+
+            if (totalTime < worldRecord)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {totalTime:F2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {totalTime - worldRecord:F2} seconds slower.");
+            }
+        }
+    }
+}
---
+++ 02.ConditionalStatements_Ex/06.WorldSwimmingRecord/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _06.WorldSwimmingRecord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordInSeconds = double.Parse(Console.ReadLine());
+            double distanceInMeters = double.Parse(Console.ReadLine());
+            double timeInSecondsPerM = double.Parse(Console.ReadLine());
+
+            double swimmingTime = timeInSecondsPerM * distanceInMeters;
+            int delaysCount = (int)Math.Floor(distanceInMeters / 15);
+            double totalDelay = delaysCount * 12.5;
+            swimmingTime += totalDelay;
+            double timeDiff = Math.Abs(swimmingTime - recordInSeconds);
+
+            if (swimmingTime < recordInSeconds)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {swimmingTime:F2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {timeDiff:F2} seconds slower.");
+            }
+        }
+    }
+}
