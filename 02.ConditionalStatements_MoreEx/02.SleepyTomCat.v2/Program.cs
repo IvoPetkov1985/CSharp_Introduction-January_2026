b861bcd7aff12e4da6f269a096c5d71099c282d7
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-24 17:38:37 +0200

    Task 2 uploaded.

---
+++ 02.ConditionalStatements_MoreEx/02.SleepyTomCat.v2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _02.SleepyTomCat.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tomPlayNorm = 30000;
+            int workdayPlay = 63;
+            int vacationDayPlay = 127;
+            int totalDaysInYear = 365;
+
+            int vacationDays = int.Parse(Console.ReadLine());
+
+            int workdays = totalDaysInYear - vacationDays;
+            int totalHoursPlay = vacationDays * vacationDayPlay + workdays * workdayPlay;
+            int timeDiff = Math.Abs(tomPlayNorm - totalHoursPlay);
+            int timeDiffInHours = timeDiff / 60;
+            int timeDiffInMinutes = timeDiff % 60;
+
+            if (totalHoursPlay > tomPlayNorm)
+            {
+                Console.WriteLine("Tom will run away");
+                Console.WriteLine("{0} hours and {1} minutes more for play", timeDiffInHours, timeDiffInMinutes);
+            }
+            else
+            {
+                Console.WriteLine("Tom sleeps well");
+                Console.WriteLine("{0} hours and {1} minutes less for play", timeDiffInHours, timeDiffInMinutes);
+            }
+        }
+    }
+}
---
+++ 02.ConditionalStatements_MoreEx/02.SleepyTomCat/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _02.SleepyTomCat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysOff = int.Parse(Console.ReadLine());
+
+            int tomPlayNorm = 30000;
+            int minutesPerWorkday = 63;
+            int minutesPerHoliday = 127;
+            int daysInYear = 365;
+
+            int workDays = daysInYear - daysOff;
+            int totalMinutesPlay = workDays * minutesPerWorkday + daysOff * minutesPerHoliday;
+            int diffInMinutes = Math.Abs(totalMinutesPlay - tomPlayNorm);
+            int hours = diffInMinutes / 60;
+            int minutes = diffInMinutes % 60;
+
+            if (totalMinutesPlay > tomPlayNorm)
+            {
+                Console.WriteLine("Tom will run away");
+                Console.WriteLine($"{hours} hours and {minutes} minutes more for play");
+            }
+            else
+            {
+                Console.WriteLine("Tom sleeps well");
+                Console.WriteLine($"{hours} hours and {minutes} minutes less for play");
+            }
+        }
+    }
+}
