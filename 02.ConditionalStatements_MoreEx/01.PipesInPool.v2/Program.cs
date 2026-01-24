0f78f6604abf59ab7cf4b4fcfffb9f78ae3cac10
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-24 17:37:33 +0200

    Task 1 uploaded.
    Two solutions.

---
+++ 02.ConditionalStatements_MoreEx/01.PipesInPool.v2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.PipesInPool.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int poolVolumeInL = int.Parse(Console.ReadLine());
+            int firstPipeDebit = int.Parse(Console.ReadLine());
+            int secondPipeDebit = int.Parse(Console.ReadLine());
+            double hoursAbsence = double.Parse(Console.ReadLine());
+
+            double firstPipeWater = firstPipeDebit * hoursAbsence;
+            double secondPipeWater = secondPipeDebit * hoursAbsence;
+            double totalWater = firstPipeWater + secondPipeWater;
+            double firstPipePercent = firstPipeWater / totalWater * 100;
+            double secondPipePercent = secondPipeWater / totalWater * 100;
+
+            if (totalWater <= poolVolumeInL)
+            {
+                double percentFull = totalWater / poolVolumeInL * 100;
+                Console.WriteLine($"The pool is {percentFull:F2}% full. Pipe 1: {firstPipePercent:F2}%. Pipe 2: {secondPipePercent:F2}%.");
+            }
+            else
+            {
+                Console.WriteLine($"For {hoursAbsence:F2} hours the pool overflows with {totalWater - poolVolumeInL:F2} liters.");
+            }
+        }
+    }
+}
---
+++ 02.ConditionalStatements_MoreEx/01.PipesInPool/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _01.PipesInPool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int poolVolume = int.Parse(Console.ReadLine());
+            int firstPipeDebitPerHour = int.Parse(Console.ReadLine());
+            int secondPipeDebitPerHour = int.Parse(Console.ReadLine());
+            double hoursAbsence = double.Parse(Console.ReadLine());
+
+            double firstPipeLiters = firstPipeDebitPerHour * hoursAbsence;
+            double secondPipeLiters = secondPipeDebitPerHour * hoursAbsence;
+            double totalLiters = firstPipeLiters + secondPipeLiters;
+            double firstPipePercentage = firstPipeLiters / totalLiters * 100;
+            double secondPipePercentage = secondPipeLiters / totalLiters * 100;
+            double totalLitersPercentage = totalLiters / poolVolume * 100;
+
+            if (totalLiters <= poolVolume)
+            {
+                Console.WriteLine($"The pool is {totalLitersPercentage:f2}% full. Pipe 1: {firstPipePercentage:f2}%. Pipe 2: {secondPipePercentage:f2}%.");
+            }
+            else
+            {
+                double overflowLiters = totalLiters - poolVolume;
+                Console.WriteLine($"For {hoursAbsence:f2} hours the pool overflows with {overflowLiters:f2} liters.");
+            }
+        }
+    }
+}
