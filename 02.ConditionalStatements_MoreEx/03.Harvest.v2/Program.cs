e2226fd159022734c095f83e635e28da709aa7a3
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-24 17:39:19 +0200

    Task 3 uploaded.
    Two solutions.

---
+++ 02.ConditionalStatements_MoreEx/03.Harvest.v2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _03.Harvest.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int winePercent = 40;
+            double grapeForLiterWine = 2.5;
+
+            int totalSqM = int.Parse(Console.ReadLine());
+            double grapePerSqM = double.Parse(Console.ReadLine());
+            int litersNeeded = int.Parse(Console.ReadLine());
+            int workersCount = int.Parse(Console.ReadLine());
+
+            double totalGrape = totalSqM * grapePerSqM;
+            double grapeForWine = totalGrape * winePercent / 100;
+            double litersWine = grapeForWine / grapeForLiterWine;
+
+            if (litersWine < litersNeeded)
+            {
+                Console.WriteLine($"It will be a tough winter! More {Math.Floor(litersNeeded - litersWine)} liters wine needed.");
+            }
+            else
+            {
+                Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(litersWine)} liters.");
+                Console.WriteLine($"{Math.Ceiling(litersWine - litersNeeded)} liters left -> {Math.Ceiling((litersWine - litersNeeded) / workersCount)} liters per person.");
+            }
+        }
+    }
+}
---
+++ 02.ConditionalStatements_MoreEx/03.Harvest/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _03.Harvest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yardInSqM = int.Parse(Console.ReadLine());
+            double grapesPerSqM = double.Parse(Console.ReadLine());
+            int litersWineNeeded = int.Parse(Console.ReadLine());
+            int workersCount = int.Parse(Console.ReadLine());
+
+            double grapesPerLiter = 2.5;
+            int winePercentage = 40;
+
+            double grapesYield = yardInSqM * grapesPerSqM;
+            double wine = grapesYield * winePercentage / 100 / grapesPerLiter;
+
+            double diff = Math.Abs(wine - litersWineNeeded);
+
+            if (wine < litersWineNeeded)
+            {
+                Console.WriteLine($"It will be a tough winter! More {Math.Floor(diff)} liters wine needed.");
+            }
+            else
+            {
+                Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(wine)} liters.");
+                Console.WriteLine($"{Math.Ceiling(diff)} liters left -> {Math.Ceiling(diff / workersCount)} liters per person.");
+            }
+        }
+    }
+}
