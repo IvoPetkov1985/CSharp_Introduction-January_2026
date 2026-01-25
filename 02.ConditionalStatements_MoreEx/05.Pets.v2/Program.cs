67d30d96a14e4731f44557fa9e678bb5dbf6b85b
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-25 09:56:41 +0200

    Task 5 uploaded.
    Two solutions.

---
+++ 02.ConditionalStatements_MoreEx/05.Pets.v2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _05.Pets.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysAbsence = int.Parse(Console.ReadLine());
+            int foodAtHomeInKg = int.Parse(Console.ReadLine());
+            double eatenByDogInKg = double.Parse(Console.ReadLine());
+            double eatenByCatInKg = double.Parse(Console.ReadLine());
+            double eatenByTortoiseInG = double.Parse(Console.ReadLine());
+
+            double eatenByTortoiseInKg = eatenByTortoiseInG * 0.001;
+            double totalEatenInKg = (eatenByDogInKg + eatenByCatInKg + eatenByTortoiseInKg) * daysAbsence;
+            double foodDiff = Math.Abs(foodAtHomeInKg - totalEatenInKg);
+
+            if (foodAtHomeInKg >= totalEatenInKg)
+            {
+                Console.WriteLine($"{Math.Floor(foodDiff)} kilos of food left.");
+            }
+            else
+            {
+                Console.WriteLine($"{Math.Ceiling(foodDiff)} more kilos of food are needed.");
+            }
+        }
+    }
+}
---
+++ 02.ConditionalStatements_MoreEx/05.Pets/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _05.Pets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysAbsence = int.Parse(Console.ReadLine());
+            double foodInKg = double.Parse(Console.ReadLine());
+            double dogFoodPerDay = double.Parse(Console.ReadLine());
+            double catFoodPerDay = double.Parse((Console.ReadLine()));
+            double tortoiseFoodPerDayInG = double.Parse((Console.ReadLine()));
+
+            double tortoiseFoodPerDay = tortoiseFoodPerDayInG / 1000;
+            double neededFoodInKg = dogFoodPerDay * daysAbsence +
+                catFoodPerDay * daysAbsence +
+                tortoiseFoodPerDay * daysAbsence;
+
+            if (foodInKg >= neededFoodInKg)
+            {
+                Console.WriteLine($"{Math.Floor(foodInKg - neededFoodInKg)} kilos of food left.");
+            }
+            else
+            {
+                Console.WriteLine($"{Math.Ceiling(neededFoodInKg - foodInKg)} more kilos of food are needed.");
+            }
+        }
+    }
+}
