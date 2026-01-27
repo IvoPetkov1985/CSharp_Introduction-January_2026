21a3821ba116aaaefa503fd1ce2fb299c845e5b1
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-27 11:09:41 +0200

    Tasks 1 and 2 uploaded.

---
+++ 03.ConditionalStatements-Adv_Lab/02.WeekendOrWorkingDay/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _02.WeekendOrWorkingDay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayOfWeek = Console.ReadLine();
+
+            string output = string.Empty;
+
+            switch (dayOfWeek)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                    output = "Working day";
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    output = "Weekend";
+                    break;
+                default:
+                    output = "Error";
+                    break;
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
