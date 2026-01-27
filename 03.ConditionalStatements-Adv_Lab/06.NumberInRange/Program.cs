df6b6cb0a48985c3e70c9e50fd8c9496d3e81f71
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-27 11:11:25 +0200

    Tasks 5, 6 and 7 uploaded.

---
+++ 03.ConditionalStatements-Adv_Lab/06.NumberInRange/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _06.NumberInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            if (inputNumber >= -100 && inputNumber <= 100 && inputNumber != 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
---
+++ 03.ConditionalStatements-Adv_Lab/07.WorkingHours/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _07.WorkingHours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double hour = double.Parse(Console.ReadLine());
+            string dayOfWeek = Console.ReadLine();
+
+            if (dayOfWeek != "Sunday" && hour >= 10 && hour <= 18)
+            {
+                Console.WriteLine("open");
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
