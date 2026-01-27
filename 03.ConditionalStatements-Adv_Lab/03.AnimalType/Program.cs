db49031040f8ff5e8df5e675b504b36a41656405
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-27 11:10:48 +0200

    Tasks 3 and 4 uploaded.
    Task 3 with two solutions.

---
+++ 03.ConditionalStatements-Adv_Lab/03.AnimalType/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _03.AnimalType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+
+            string output = string.Empty;
+
+            switch (animal)
+            {
+                case "dog":
+                    output = "mammal";
+                    break;
+                case "crocodile":
+                case "tortoise":
+                case "snake":
+                    output = "reptile";
+                    break;
+                default:
+                    output = "unknown";
+                    break;
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ 03.ConditionalStatements-Adv_Lab/04.PersonalTitles/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _04.PersonalTitles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            char sex = char.Parse(Console.ReadLine());
+
+            string title = string.Empty;
+
+            switch (sex)
+            {
+                case 'm':
+
+                    if (age >= 16)
+                    {
+                        title = "Mr.";
+                    }
+                    else
+                    {
+                        title = "Master";
+                    }
+                    break;
+
+                case 'f':
+
+                    if (age >= 16)
+                    {
+                        title = "Ms.";
+                    }
+                    else
+                    {
+                        title = "Miss";
+                    }
+                    break;
+            }
+
+            Console.WriteLine(title);
+        }
+    }
+}
