83ce9181a99832759f9dd65f105eeb38f7e97fd6
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-28 12:35:41 +0200

    Task 11 uploaded.
    Two solutions.

---
+++ 03.ConditionalStatements-Adv_Lab/11.FruitShop.v2/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _11.FruitShop.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string dayOfWeek = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+
+            double pricePerKg = 0;
+            bool isFruitValid = true;
+            bool isDayValid = true;
+
+            if (dayOfWeek != "Monday" &&
+                dayOfWeek != "Tuesday" &&
+                dayOfWeek != "Wednesday" &&
+                dayOfWeek != "Thursday" &&
+                dayOfWeek != "Friday" &&
+                dayOfWeek != "Saturday" &&
+                dayOfWeek != "Sunday")
+            {
+                isDayValid = false;
+            }
+
+            switch (dayOfWeek)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+
+                    switch (fruit)
+                    {
+                        case "banana": pricePerKg = 2.50; break;
+                        case "apple": pricePerKg = 1.20; break;
+                        case "orange": pricePerKg = 0.85; break;
+                        case "grapefruit": pricePerKg = 1.45; break;
+                        case "kiwi": pricePerKg = 2.70; break;
+                        case "pineapple": pricePerKg = 5.50; break;
+                        case "grapes": pricePerKg = 3.85; break;
+                        default: isFruitValid = false; break;
+                    }
+
+                    break;
+                case "Saturday":
+                case "Sunday":
+
+                    switch (fruit)
+                    {
+                        case "banana": pricePerKg = 2.70; break;
+                        case "apple": pricePerKg = 1.25; break;
+                        case "orange": pricePerKg = 0.90; break;
+                        case "grapefruit": pricePerKg = 1.60; break;
+                        case "kiwi": pricePerKg = 3.00; break;
+                        case "pineapple": pricePerKg = 5.60; break;
+                        case "grapes": pricePerKg = 4.20; break;
+                        default: isFruitValid = false; break;
+                    }
+
+                    break;
+            }
+
+            if (isFruitValid && isDayValid)
+            {
+                double finalPrice = pricePerKg * quantity;
+                Console.WriteLine("{0:F2}", finalPrice);
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
---
+++ 03.ConditionalStatements-Adv_Lab/11.FruitShop/Program.cs
@@ -0,0 +1,154 @@
+﻿namespace _11.FruitShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string dayOfWeek = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+
+            double pricePerKilo = 0;
+            bool isDayValid = true;
+            bool isFruitValid = true;
+
+            switch (fruit)
+            {
+                case "banana":
+
+                    if (dayOfWeek == "Monday" || dayOfWeek == "Tuesday" || dayOfWeek == "Wednesday" || dayOfWeek == "Thursday" || dayOfWeek == "Friday")
+                    {
+                        pricePerKilo = 2.50;
+                    }
+                    else if (dayOfWeek == "Saturday" || dayOfWeek == "Sunday")
+                    {
+                        pricePerKilo = 2.70;
+                    }
+                    else
+                    {
+                        isDayValid = false;
+                    }
+
+                    break;
+
+                case "apple":
+
+                    if (dayOfWeek == "Monday" || dayOfWeek == "Tuesday" || dayOfWeek == "Wednesday" || dayOfWeek == "Thursday" || dayOfWeek == "Friday")
+                    {
+                        pricePerKilo = 1.20;
+                    }
+                    else if (dayOfWeek == "Saturday" || dayOfWeek == "Sunday")
+                    {
+                        pricePerKilo = 1.25;
+                    }
+                    else
+                    {
+                        isDayValid = false;
+                    }
+
+                    break;
+
+                case "orange":
+
+                    if (dayOfWeek == "Monday" || dayOfWeek == "Tuesday" || dayOfWeek == "Wednesday" || dayOfWeek == "Thursday" || dayOfWeek == "Friday")
+                    {
+                        pricePerKilo = 0.85;
+                    }
+                    else if (dayOfWeek == "Saturday" || dayOfWeek == "Sunday")
+                    {
+                        pricePerKilo = 0.90;
+                    }
+                    else
+                    {
+                        isDayValid = false;
+                    }
+
+                    break;
+
+                case "grapefruit":
+
+                    if (dayOfWeek == "Monday" || dayOfWeek == "Tuesday" || dayOfWeek == "Wednesday" || dayOfWeek == "Thursday" || dayOfWeek == "Friday")
+                    {
+                        pricePerKilo = 1.45;
+                    }
+                    else if (dayOfWeek == "Saturday" || dayOfWeek == "Sunday")
+                    {
+                        pricePerKilo = 1.60;
+                    }
+                    else
+                    {
+                        isDayValid = false;
+                    }
+
+                    break;
+
+                case "kiwi":
+
+                    if (dayOfWeek == "Monday" || dayOfWeek == "Tuesday" || dayOfWeek == "Wednesday" || dayOfWeek == "Thursday" || dayOfWeek == "Friday")
+                    {
+                        pricePerKilo = 2.70;
+                    }
+                    else if (dayOfWeek == "Saturday" || dayOfWeek == "Sunday")
+                    {
+                        pricePerKilo = 3.00;
+                    }
+                    else
+                    {
+                        isDayValid = false;
+                    }
+
+                    break;
+
+                case "pineapple":
+
+                    if (dayOfWeek == "Monday" || dayOfWeek == "Tuesday" || dayOfWeek == "Wednesday" || dayOfWeek == "Thursday" || dayOfWeek == "Friday")
+                    {
+                        pricePerKilo = 5.50;
+                    }
+                    else if (dayOfWeek == "Saturday" || dayOfWeek == "Sunday")
+                    {
+                        pricePerKilo = 5.60;
+                    }
+                    else
+                    {
+                        isDayValid = false;
+                    }
+
+                    break;
+
+                case "grapes":
+
+                    if (dayOfWeek == "Monday" || dayOfWeek == "Tuesday" || dayOfWeek == "Wednesday" || dayOfWeek == "Thursday" || dayOfWeek == "Friday")
+                    {
+                        pricePerKilo = 3.85;
+                    }
+                    else if (dayOfWeek == "Saturday" || dayOfWeek == "Sunday")
+                    {
+                        pricePerKilo = 4.20;
+                    }
+                    else
+                    {
+                        isDayValid = false;
+                    }
+
+                    break;
+
+                default:
+
+                    isFruitValid = false;
+
+                    break;
+            }
+
+            if (isFruitValid && isDayValid)
+            {
+                double finalPrice = pricePerKilo * quantity;
+                Console.WriteLine($"{finalPrice:F2}");
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
