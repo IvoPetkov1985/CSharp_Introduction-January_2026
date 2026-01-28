118b3c1c913a910f152f19484e138e5c5a34b4f5
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-28 12:36:23 +0200

    Task 12 uploaded.
    Two solutions.

---
+++ 03.ConditionalStatements-Adv_Lab/12.TradeCommissions.v2/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace _12.TradeCommissions.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+
+            bool isCityValid = true;
+            bool areSalesValid = true;
+            double multiplyer = 0;
+
+            if (sales < 0)
+            {
+                areSalesValid = false;
+            }
+
+            if (city == "Sofia")
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    multiplyer = 0.05;
+                }
+                else if (sales > 500 && sales <= 1000)
+                {
+                    multiplyer = 0.07;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    multiplyer = 0.08;
+                }
+                else if (sales > 10000)
+                {
+                    multiplyer = 0.12;
+                }
+            }
+            else if (city == "Varna")
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    multiplyer = 0.045;
+                }
+                else if (sales > 500 && sales <= 1000)
+                {
+                    multiplyer = 0.075;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    multiplyer = 0.10;
+                }
+                else if (sales > 10000)
+                {
+                    multiplyer = 0.13;
+                }
+            }
+            else if (city == "Plovdiv")
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    multiplyer = 0.055;
+                }
+                else if (sales > 500 && sales <= 1000)
+                {
+                    multiplyer = 0.08;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    multiplyer = 0.12;
+                }
+                else if (sales > 10000)
+                {
+                    multiplyer = 0.145;
+                }
+            }
+            else
+            {
+                isCityValid = false;
+            }
+
+            if (!areSalesValid || !isCityValid)
+            {
+                Console.WriteLine("error");
+            }
+            else
+            {
+                double tradeCommission = sales * multiplyer;
+                Console.WriteLine("{0:F2}", tradeCommission);
+            }
+        }
+    }
+}
---
+++ 03.ConditionalStatements-Adv_Lab/12.TradeCommissions/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace _12.TradeCommissions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+
+            double commission = 0;
+            bool isCityValid = true;
+
+            switch (city)
+            {
+                case "Sofia":
+
+                    if (sales >= 0 && sales <= 500)
+                    {
+                        commission = 5;
+                    }
+                    else if (sales > 500 && sales <= 1000)
+                    {
+                        commission = 7;
+                    }
+                    else if (sales > 1000 && sales <= 10000)
+                    {
+                        commission = 8;
+                    }
+                    else if (sales > 10000)
+                    {
+                        commission = 12;
+                    }
+
+                    break;
+
+                case "Plovdiv":
+
+                    if (sales >= 0 && sales <= 500)
+                    {
+                        commission = 5.5;
+                    }
+                    else if (sales > 500 && sales <= 1000)
+                    {
+                        commission = 8;
+                    }
+                    else if (sales > 1000 && sales <= 10000)
+                    {
+                        commission = 12;
+                    }
+                    else if (sales > 10000)
+                    {
+                        commission = 14.5;
+                    }
+
+                    break;
+
+                case "Varna":
+
+                    if (sales >= 0 && sales <= 500)
+                    {
+                        commission = 4.5;
+                    }
+                    else if (sales > 500 && sales <= 1000)
+                    {
+                        commission = 7.5;
+                    }
+                    else if (sales > 1000 && sales <= 10000)
+                    {
+                        commission = 10;
+                    }
+                    else if (sales > 10000)
+                    {
+                        commission = 13;
+                    }
+
+                    break;
+
+                default:
+                    isCityValid = false;
+                    break;
+            }
+
+            if (isCityValid && sales >= 0)
+            {
+                double tradeCommission = sales * commission / 100;
+                Console.WriteLine($"{tradeCommission:F2}");
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
