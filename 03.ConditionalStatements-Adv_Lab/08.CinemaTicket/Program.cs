fdb02b9ab2f9d6df291245a72b51fd2dd7b71dba
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-28 12:35:01 +0200

    Tasks 8, 9 and 10 uploaded.

---
+++ 03.ConditionalStatements-Adv_Lab/08.CinemaTicket/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _08.CinemaTicket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayOfWeek = Console.ReadLine();
+            double ticketPrice = 0;
+
+            switch (dayOfWeek)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Friday":
+                    ticketPrice = 12;
+                    break;
+                case "Wednesday":
+                case "Thursday":
+                    ticketPrice = 14;
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    ticketPrice = 16; 
+                    break;
+            }
+
+            Console.WriteLine(ticketPrice);
+        }
+    }
+}
---
+++ 03.ConditionalStatements-Adv_Lab/10.InvalidNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _10.InvalidNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            bool isValid = (inputNumber >= 100 && inputNumber <= 200) || inputNumber == 0;
+
+            if (isValid == false)
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
