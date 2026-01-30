8fbbc74d8c2269c971ef3cc75b10b2450c23d9c8
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-30 10:54:52 +0200

    Tasks 1 and 2 uploaded.

---
+++ 03.Conditional Statements Adv_Ex/01.Cinema/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01.Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string projectionType = Console.ReadLine();
+            int rowsCount = int.Parse(Console.ReadLine());
+            int colsCount = int.Parse(Console.ReadLine());
+
+            double ticketPrice = 0;
+            int attenders = rowsCount * colsCount;
+
+            switch (projectionType)
+            {
+                case "Premiere": ticketPrice = 12.00; break;
+                case "Normal": ticketPrice = 7.50; break;
+                case "Discount": ticketPrice = 5.00; break;
+            }
+
+            double income = ticketPrice * attenders;
+            Console.WriteLine($"{income:F2} leva");
+        }
+    }
+}
---
+++ 03.Conditional Statements Adv_Ex/02.SummerOutfit/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace _02.SummerOutfit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int degrees = int.Parse(Console.ReadLine());
+            string partOfDay = Console.ReadLine();
+
+            string outfit = string.Empty;
+            string shoes = string.Empty;
+
+            switch (partOfDay)
+            {
+                case "Morning":
+
+                    if (degrees <= 18)
+                    {
+                        outfit = "Sweatshirt";
+                        shoes = "Sneakers";
+                    }
+                    else if (degrees <= 24)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    else if (degrees > 24)
+                    {
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                    }
+
+                    break;
+
+                case "Afternoon":
+
+                    if (degrees <= 18)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    else if (degrees <= 24)
+                    {
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                    }
+                    else if (degrees > 24)
+                    {
+                        outfit = "Swim Suit";
+                        shoes = "Barefoot";
+                    }
+
+                    break;
+
+                case "Evening":
+
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+
+                    break;
+            }
+
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+        }
+    }
+}
