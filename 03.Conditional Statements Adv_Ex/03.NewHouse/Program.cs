8438c00ad0bf5dc01aa51c023e9f7e4849a4e6e1
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-30 10:55:44 +0200

    Tasks 3 and 4 uploaded.

---
+++ 03.Conditional Statements Adv_Ex/03.NewHouse/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _03.NewHouse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string flowerType = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+            int budget = int.Parse(Console.ReadLine());
+
+            double singlePrice = 0;
+
+            switch (flowerType)
+            {
+                case "Roses": singlePrice = 5.00; break;
+                case "Dahlias": singlePrice = 3.80; break;
+                case "Tulips": singlePrice = 2.80; break;
+                case "Narcissus": singlePrice = 3.00; break;
+                case "Gladiolus": singlePrice = 2.50; break;
+            }
+
+            double finalPrice = singlePrice * count;
+
+            if (flowerType == "Roses" && count > 80)
+            {
+                finalPrice *= 0.90;
+            }
+            else if (flowerType == "Dahlias" && count > 90)
+            {
+                finalPrice *= 0.85;
+            }
+            else if (flowerType == "Tulips" && count > 80)
+            {
+                finalPrice *= 0.85;
+            }
+            else if (flowerType == "Narcissus" && count < 120)
+            {
+                finalPrice *= 1.15;
+            }
+            else if (flowerType == "Gladiolus" && count < 80)
+            {
+                finalPrice *= 1.20;
+            }
+
+            if (budget >= finalPrice)
+            {
+                Console.WriteLine($"Hey, you have a great garden with {count} {flowerType} and {budget - finalPrice:F2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money, you need {finalPrice - budget:F2} leva more.");
+            }
+        }
+    }
+}
---
+++ 03.Conditional Statements Adv_Ex/04.FishingBoat/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _04.FishingBoat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int groupBudget = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            int fishermen = int.Parse(Console.ReadLine());
+
+            double boatRent = 0;
+
+            switch (season)
+            {
+                case "Spring": boatRent = 3000; break;
+                case "Summer":
+                case "Autumn": boatRent = 4200; break;
+                case "Winter": boatRent = 2600; break;
+            }
+
+            if (fishermen <= 6)
+            {
+                boatRent *= 0.90;
+            }
+            else if (fishermen <= 11)
+            {
+                boatRent *= 0.85;
+            }
+            else if (fishermen >= 12)
+            {
+                boatRent *= 0.75;
+            }
+
+            if (season != "Autumn" && fishermen % 2 == 0)
+            {
+                boatRent *= 0.95;
+            }
+
+            if (groupBudget >= boatRent)
+            {
+                Console.WriteLine($"Yes! You have {groupBudget - boatRent:F2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {boatRent - groupBudget:F2} leva.");
+            }
+        }
+    }
+}
