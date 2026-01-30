f4cbd08017ad6f1a9c03b7f514fd4d37dac79556
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-30 10:56:14 +0200

    Tasks 5 and 6 uploaded.

---
+++ 03.Conditional Statements Adv_Ex/05.Journey/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _05.Journey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            string destination = string.Empty;
+            string typeOfStay = string.Empty;
+            double spentMoney = 0;
+
+            if (budget <= 100)
+            {
+                destination = "Bulgaria";
+
+                switch (season)
+                {
+                    case "summer": spentMoney = budget * 0.3; typeOfStay = "Camp"; break;
+                    case "winter": spentMoney = budget * 0.7; typeOfStay = "Hotel"; break;
+                }
+            }
+            else if (budget <= 1000)
+            {
+                destination = "Balkans";
+
+                switch (season)
+                {
+                    case "summer": spentMoney = budget * 0.4; typeOfStay = "Camp"; break;
+                    case "winter": spentMoney = budget * 0.8; typeOfStay = "Hotel"; break;
+                }
+            }
+            else
+            {
+                destination = "Europe";
+                spentMoney = budget * 0.9;
+                typeOfStay = "Hotel";
+            }
+
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{typeOfStay} - {spentMoney:F2}");
+        }
+    }
+}
---
+++ 03.Conditional Statements Adv_Ex/06.OperationsBetweenNumbers/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace _06.OperationsBetweenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            char operation = char.Parse(Console.ReadLine());
+
+            double result = 0;
+            string evenOrOdd = string.Empty;
+
+            switch (operation)
+            {
+                case '+':
+                case '-':
+                case '*':
+
+                    if (operation == '+')
+                    {
+                        result = n1 + n2;
+                    }
+                    else if (operation == '-')
+                    {
+                        result = n1 - n2;
+                    }
+                    else if (operation == '*')
+                    {
+                        result = n2 * n1;
+                    }
+
+                    if (result % 2 == 0)
+                    {
+                        evenOrOdd = "even";
+                    }
+                    else
+                    {
+                        evenOrOdd = "odd";
+                    }
+
+                    Console.WriteLine($"{n1} {operation} {n2} = {result} - {evenOrOdd}");
+                    break;
+
+                case '/':
+
+                    if (n2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {n1} by zero");
+                    }
+                    else
+                    {
+                        result = (double)n1 / n2;
+                        Console.WriteLine($"{n1} {operation} {n2} = {result:F2}");
+                    }
+
+                    break;
+
+                case '%':
+
+                    if (n2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {n1} by zero");
+                    }
+                    else
+                    {
+                        result = n1 % n2;
+                        Console.WriteLine($"{n1} {operation} {n2} = {result}");
+                    }
+
+                    break;
+            }
+        }
+    }
+}
