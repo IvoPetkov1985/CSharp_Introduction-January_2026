48eadd6d4233626f7f2f6060fce70a58323c0e5a
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-31 08:46:28 +0200

    The last 9 uploaded.
    Two solutions.

---
+++ 03.Conditional Statements Adv_Ex/09.SkiTrip.v2/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace _09.SkiTrip.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysCount = int.Parse(Console.ReadLine());
+            string roomType = Console.ReadLine();
+            string estimation = Console.ReadLine();
+
+            double singlePrice = 0;
+
+            if (roomType == "room for one person")
+            {
+                singlePrice = 18;
+            }
+            else if (roomType == "apartment")
+            {
+                singlePrice = 25;
+            }
+            else if (roomType == "president apartment")
+            {
+                singlePrice = 35;
+            }
+
+            int nights = daysCount - 1;
+            double totalPrice = singlePrice * nights;
+
+            if (roomType == "apartment")
+            {
+                if (daysCount > 15)
+                {
+                    totalPrice *= 0.5;
+                }
+                else if (daysCount >= 10)
+                {
+                    totalPrice *= 1 - 0.35;
+                }
+                else
+                {
+                    totalPrice *= 1 - 0.3;
+                }
+            }
+            else if (roomType == "president apartment")
+            {
+                if (daysCount > 15)
+                {
+                    totalPrice *= 1 - 0.2;
+                }
+                else if (daysCount >= 10)
+                {
+                    totalPrice *= 1 - 0.15;
+                }
+                else
+                {
+                    totalPrice *= 1 - 0.1;
+                }
+            }
+
+            if (estimation == "positive")
+            {
+                totalPrice *= 1.25;
+            }
+            else
+            {
+                totalPrice *= 1 - 0.1;
+            }
+
+            Console.WriteLine($"{totalPrice:F2}");
+        }
+    }
+}
---
+++ 03.Conditional Statements Adv_Ex/09.SkiTrip/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace _09.SkiTrip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            string roomType = Console.ReadLine();
+            string estimation = Console.ReadLine();
+
+            double pricePerNight = 0;
+
+            switch (roomType)
+            {
+                case "room for one person":
+
+                    pricePerNight = 18.00;
+                    break;
+
+                case "apartment":
+
+                    pricePerNight = 25.00;
+
+                    if (days > 15)
+                    {
+                        pricePerNight *= 0.5;
+                    }
+                    else if (days >= 10)
+                    {
+                        pricePerNight *= 0.65;
+                    }
+                    else
+                    {
+                        pricePerNight *= 0.7;
+                    }
+
+                    break;
+
+                case "president apartment":
+
+                    pricePerNight = 35.00;
+
+                    if (days > 15)
+                    {
+                        pricePerNight *= 0.8;
+                    }
+                    else if (days >= 10)
+                    {
+                        pricePerNight *= 0.85;
+                    }
+                    else
+                    {
+                        pricePerNight *= 0.9;
+                    }
+
+                    break;
+            }
+
+            int nights = days - 1;
+            double priceForStay = pricePerNight * nights;
+
+            switch (estimation)
+            {
+                case "positive":
+
+                    priceForStay *= 1.25;
+                    break;
+
+                case "negative":
+
+                    priceForStay *= 0.90;
+                    break;
+            }
+
+            Console.WriteLine($"{priceForStay:F2}");
+        }
+    }
+}
