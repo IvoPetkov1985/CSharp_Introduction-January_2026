41e27afd0e2083ada71d6f25f9c29f754918f8c1
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-01-31 08:45:03 +0200

    Task 6 uploaded.
    Another solution.

---
+++ 03.Conditional Statements Adv_Ex/06.OperationsBetweenNumbers.v2/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _06.OperationsBetweenNumbers.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            string oper = Console.ReadLine();
+
+            double result = 0;
+            bool isDivisorZero = secondNum == 0;
+
+            if (oper == "+")
+            {
+                result = firstNum + secondNum;
+            }
+            else if (oper == "-")
+            {
+                result = firstNum - secondNum;
+            }
+            else if (oper == "*")
+            {
+                result = firstNum * secondNum;
+            }
+            else if (oper == "/")
+            {
+                if (isDivisorZero == false)
+                {
+                    result = firstNum / (secondNum * 1.0);
+                }
+            }
+            else if (oper == "%")
+            {
+                if (isDivisorZero == false)
+                {
+                    result = firstNum % secondNum;
+                }
+            }
+
+            if (oper == "+" || oper == "-" || oper == "*")
+            {
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine($"{firstNum} {oper} {secondNum} = {result} - even");
+                }
+                else
+                {
+                    Console.WriteLine($"{firstNum} {oper} {secondNum} = {result} - odd");
+                }
+            }
+            else if (oper == "/")
+            {
+                if (isDivisorZero)
+                {
+                    Console.WriteLine($"Cannot divide {firstNum} by zero");
+                }
+                else
+                {
+                    Console.WriteLine($"{firstNum} / {secondNum} = {result:F2}");
+                }
+            }
+            else if (oper == "%")
+            {
+                if (isDivisorZero)
+                {
+                    Console.WriteLine($"Cannot divide {firstNum} by zero");
+                }
+                else
+                {
+                    Console.WriteLine($"{firstNum} % {secondNum} = {result}");
+                }
+            }
+        }
+    }
+}
