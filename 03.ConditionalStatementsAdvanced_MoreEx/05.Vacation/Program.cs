5c7c669c9a66dffec0f9f6ffa464c83f0ce9b3fd
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-01 14:47:11 +0200

    Tasks 5 and 6 uploaded.

---
+++ 03.ConditionalStatementsAdvanced_MoreEx/05.Vacation/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace _05.Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            string location = string.Empty;
+            string typeOfStay = string.Empty;
+            double priceOfStay = 0;
+
+            if (budget <= 1000)
+            {
+                typeOfStay = "Camp";
+
+                switch (season)
+                {
+                    case "Summer":
+                        location = "Alaska";
+                        priceOfStay = budget * 0.65;
+                        break;
+                    case "Winter":
+                        location = "Morocco";
+                        priceOfStay = budget * 0.45;
+                        break;
+                }
+            }
+            else if (budget <= 3000)
+            {
+                typeOfStay = "Hut";
+
+                switch (season)
+                {
+                    case "Summer":
+                        location = "Alaska";
+                        priceOfStay = budget * 0.80;
+                        break;
+                    case "Winter":
+                        location = "Morocco";
+                        priceOfStay = budget * 0.60;
+                        break;
+                }
+            }
+            else
+            {
+                typeOfStay = "Hotel";
+                priceOfStay = budget * 0.90;
+
+                switch (season)
+                {
+                    case "Summer":
+                        location = "Alaska";
+                        break;
+                    case "Winter":
+                        location = "Morocco";
+                        break;
+                }
+            }
+
+            Console.WriteLine($"{location} - {typeOfStay} - {priceOfStay:F2}");
+        }
+    }
+}
---
+++ 03.ConditionalStatementsAdvanced_MoreEx/06.TruckDriver/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace _06.TruckDriver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string season = Console.ReadLine();
+            double kmPerMonth = double.Parse(Console.ReadLine());
+
+            int monthsWork = 4;
+            double pricePerKm = 0;
+
+            if (kmPerMonth <= 5000)
+            {
+                switch (season)
+                {
+                    case "Spring":
+                    case "Autumn":
+                        pricePerKm = 0.75;
+                        break;
+                    case "Summer":
+                        pricePerKm = 0.90;
+                        break;
+                    case "Winter":
+                        pricePerKm = 1.05;
+                        break;
+                }
+            }
+            else if (kmPerMonth <= 10000)
+            {
+                switch (season)
+                {
+                    case "Spring":
+                    case "Autumn":
+                        pricePerKm = 0.95;
+                        break;
+                    case "Summer":
+                        pricePerKm = 1.10;
+                        break;
+                    case "Winter":
+                        pricePerKm = 1.25;
+                        break;
+                }
+            }
+            else
+            {
+                pricePerKm = 1.45;
+            }
+
+            double salaryForMonth = kmPerMonth * pricePerKm;
+            double salaryForSeason = salaryForMonth * monthsWork;
+            double salaryNetto = salaryForSeason * (1 - 0.10);
+            Console.WriteLine($"{salaryNetto:F2}");
+        }
+    }
+}
