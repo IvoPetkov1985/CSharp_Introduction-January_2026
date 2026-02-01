7ee95be4ff31ae85b8caaec643d0268448438262
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-01 14:45:38 +0200

    Tasks 1 and 2 uploaded.

---
+++ 03.ConditionalStatementsAdvanced_MoreEx/01.MatchTickets/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _01.MatchTickets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string category = Console.ReadLine();
+            int fansCount = int.Parse(Console.ReadLine());
+
+            double ticketPrice = 0;
+            double transportPrice = 0;
+
+            if (category == "VIP")
+            {
+                ticketPrice = 499.99;
+            }
+            else if (category == "Normal")
+            {
+                ticketPrice = 249.99;
+            }
+
+            if (fansCount <= 4)
+            {
+                transportPrice = budget * 0.75;
+            }
+            else if (fansCount <= 9)
+            {
+                transportPrice = budget * 0.60;
+            }
+            else if (fansCount <= 24)
+            {
+                transportPrice = budget * 0.50;
+            }
+            else if (fansCount <= 49)
+            {
+                transportPrice = budget * 0.40;
+            }
+            else
+            {
+                transportPrice = budget * 0.25;
+            }
+
+            double moneyForTickets = budget - transportPrice;
+            double priceForTickets = ticketPrice * fansCount;
+
+            if (moneyForTickets >= priceForTickets)
+            {
+                Console.WriteLine($"Yes! You have {moneyForTickets - priceForTickets:F2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {priceForTickets - moneyForTickets:F2} leva.");
+            }
+        }
+    }
+}
---
+++ 03.ConditionalStatementsAdvanced_MoreEx/02.BikeRace/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _02.BikeRace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int juniorsCount = int.Parse(Console.ReadLine());
+            int seniorsCount = int.Parse(Console.ReadLine());
+            string traceType = Console.ReadLine();
+
+            double juniorTicketPrice = 0;
+            double seniorTicketPrice = 0;
+
+            switch (traceType)
+            {
+                case "trail":
+                    juniorTicketPrice = 5.50;
+                    seniorTicketPrice = 7.00;
+                    break;
+                case "cross-country":
+                    juniorTicketPrice = 8.00;
+                    seniorTicketPrice = 9.50;
+
+                    if (juniorsCount + seniorsCount >= 50)
+                    {
+                        juniorTicketPrice *= 0.75;
+                        seniorTicketPrice *= 0.75;
+                    }
+
+                    break;
+                case "downhill":
+                    juniorTicketPrice = 12.25;
+                    seniorTicketPrice = 13.75;
+                    break;
+                case "road":
+                    juniorTicketPrice = 20.00;
+                    seniorTicketPrice = 21.50;
+                    break;
+            }
+
+            double moneyFromTickets = juniorsCount * juniorTicketPrice + seniorsCount * seniorTicketPrice;
+            double moneyWithoutExpenses = moneyFromTickets * 0.95;
+
+            Console.WriteLine($"{moneyWithoutExpenses:F2}");
+        }
+    }
+}
