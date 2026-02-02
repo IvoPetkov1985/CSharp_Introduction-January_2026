91791ea40fa8704346c52b4ea49d0e5c93a3fa28
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-02 10:18:32 +0200

    Tasks 7 and 8 uploaded.
    With two solutions.

---
+++ 03.ConditionalStatementsAdvanced_MoreEx/07.SchoolCamp.v2/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace _07.SchoolCamp.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string season = Console.ReadLine();
+            string groupType = Console.ReadLine();
+            int studentsCount = int.Parse(Console.ReadLine());
+            int nightsCount = int.Parse(Console.ReadLine());
+
+            string sport = string.Empty;
+            double nightPrice = 0;
+
+            if (groupType == "girls")
+            {
+                if (season == "Winter")
+                {
+                    nightPrice = 9.60;
+                    sport = "Gymnastics";
+                }
+                else if (season == "Spring")
+                {
+                    nightPrice = 7.20;
+                    sport = "Athletics";
+                }
+                else if (season == "Summer")
+                {
+                    nightPrice = 15.00;
+                    sport = "Volleyball";
+                }
+            }
+            else if (groupType == "boys")
+            {
+                if (season == "Winter")
+                {
+                    nightPrice = 9.60;
+                    sport = "Judo";
+                }
+                else if (season == "Spring")
+                {
+                    nightPrice = 7.20;
+                    sport = "Tennis";
+                }
+                else if (season == "Summer")
+                {
+                    nightPrice = 15.00;
+                    sport = "Football";
+                }
+            }
+            else if (groupType == "mixed")
+            {
+                if (season == "Winter")
+                {
+                    nightPrice = 10.00;
+                    sport = "Ski";
+                }
+                else if (season == "Spring")
+                {
+                    nightPrice = 9.50;
+                    sport = "Cycling";
+                }
+                else if (season == "Summer")
+                {
+                    nightPrice = 20.00;
+                    sport = "Swimming";
+                }
+            }
+
+            double finalSum = nightsCount * nightPrice * studentsCount;
+
+            if (studentsCount >= 10 && studentsCount < 20)
+            {
+                finalSum *= 0.95;
+            }
+            else if (studentsCount >= 20 && studentsCount < 50)
+            {
+                finalSum *= 0.85;
+            }
+            else if (studentsCount >= 50)
+            {
+                finalSum /= 2;
+            }
+
+            Console.WriteLine($"{sport} {finalSum:F2} lv.");
+        }
+    }
+}
---
+++ 03.ConditionalStatementsAdvanced_MoreEx/07.SchoolCamp/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace _07.SchoolCamp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string season = Console.ReadLine();
+            string groupType = Console.ReadLine();
+            int studentsCount = int.Parse(Console.ReadLine());
+            int nights = int.Parse(Console.ReadLine());
+
+            double pricePerNight = 0;
+            string sport = string.Empty;
+
+            switch (season)
+            {
+                case "Winter":
+
+                    switch (groupType)
+                    {
+                        case "boys":
+                            sport = "Judo";
+                            pricePerNight = 9.60;
+                            break;
+                        case "girls":
+                            sport = "Gymnastics";
+                            pricePerNight = 9.60;
+                            break;
+                        case "mixed":
+                            sport = "Ski";
+                            pricePerNight = 10.00;
+                            break;
+                    }
+
+                    break;
+
+                case "Spring":
+
+                    switch (groupType)
+                    {
+                        case "boys":
+                            sport = "Tennis";
+                            pricePerNight = 7.20;
+                            break;
+                        case "girls":
+                            sport = "Athletics";
+                            pricePerNight = 7.20;
+                            break;
+                        case "mixed":
+                            sport = "Cycling";
+                            pricePerNight = 9.50;
+                            break;
+                    }
+
+                    break;
+
+                case "Summer":
+
+                    switch (groupType)
+                    {
+                        case "boys":
+                            sport = "Football";
+                            pricePerNight = 15.00;
+                            break;
+                        case "girls":
+                            sport = "Volleyball";
+                            pricePerNight = 15.00;
+                            break;
+                        case "mixed":
+                            sport = "Swimming";
+                            pricePerNight = 20.00;
+                            break;
+                    }
+
+                    break;
+            }
+
+            double totalPrice = nights * pricePerNight * studentsCount;
+
+            if (studentsCount >= 10 && studentsCount < 20)
+            {
+                totalPrice *= 0.95;
+            }
+            else if (studentsCount >= 20 && studentsCount < 50)
+            {
+                totalPrice *= 0.85;
+            }
+            else if (studentsCount >= 50)
+            {
+                totalPrice /= 2;
+            }
+
+            Console.WriteLine($"{sport} {totalPrice:F2} lv.");
+        }
+    }
+}
