5ded59007c0e77498ff0132a87c37d5bf56bc256
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-03 12:01:05 +0200

    Task 5 and .sln file uploaded.

---
+++ 04.ForLoop_Lab/05.CharacterSequence/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _05.CharacterSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int length = text.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                char symbol = text[i];
+                Console.WriteLine(symbol);
+            }
+        }
+    }
+}
