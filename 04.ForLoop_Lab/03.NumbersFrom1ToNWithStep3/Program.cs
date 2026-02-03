56592388a6b6a051cf94b01999d6478906fe20f2
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-03 12:00:33 +0200

    Tasks 3 and 4 uploaded.

---
+++ 04.ForLoop_Lab/03.NumbersFrom1ToNWithStep3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _03.NumbersFrom1ToNWithStep3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int step = 3;
+
+            for (int i = 1; i <= number; i += step)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ 04.ForLoop_Lab/04.EvenPowersOf2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _04.EvenPowersOf2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int step = 2;
+
+            for (int i = 0; i <= number; i += step)
+            {
+                double result = Math.Pow(2, i);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
