3388c00df67e7711907767f7aaf1fc10e54aa7f6
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-04 11:15:07 +0200

    Task 8 uploaded.

---
+++ 04.ForLoop_Lab/08.NumberSequence/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _08.NumberSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            int minNumber = int.MaxValue;
+            int maxNumber = int.MinValue;
+
+            for (int i = 0; i < count; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+
+                if (currentNumber < minNumber)
+                {
+                    minNumber = currentNumber;
+                }
+
+                if (currentNumber > maxNumber)
+                {
+                    maxNumber = currentNumber;
+                }
+            }
+
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.WriteLine($"Min number: {minNumber}");
+        }
+    }
+}
