2113e114b70b12fc027a310e69d639dd88f98e03
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-04 11:14:38 +0200

    Tasks 6 and 7 uploaded.

---
+++ 04.ForLoop_Lab/06.VowelsSum/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _06.VowelsSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int length = text.Length;
+            int sum = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                char symbol = text[i];
+
+                switch (symbol)
+                {
+                    case 'a': sum += 1; break;
+                    case 'e': sum += 2; break;
+                    case 'i': sum += 3; break;
+                    case 'o': sum += 4; break;
+                    case 'u': sum += 5; break;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
