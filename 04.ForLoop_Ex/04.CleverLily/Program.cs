d1c940ec65386b5246aac15394b879e4b769557c
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-05 10:49:43 +0200

    Tasks 4 and 5 uploaded.

---
+++ 04.ForLoop_Ex/04.CleverLily/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _04.CleverLily
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lilyAge = int.Parse(Console.ReadLine());
+            double washingMachinePrice = double.Parse(Console.ReadLine());
+            int toyPrice = int.Parse(Console.ReadLine());
+
+            int birthdayMoney = 10;
+            int moneyFromBirthdays = 0;
+            int toysCount = 0;
+            int brotherMoney = 0;
+
+            for (int i = 1; i <= lilyAge; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    moneyFromBirthdays += birthdayMoney;
+                    birthdayMoney += 10;
+                    brotherMoney++;
+                }
+                else
+                {
+                    toysCount++;
+                }
+            }
+
+            int savedMoney = moneyFromBirthdays - brotherMoney;
+            int moneyFromToys = toysCount * toyPrice;
+            int totalMoney = savedMoney + moneyFromToys;
+
+            if (totalMoney >= washingMachinePrice)
+            {
+                Console.WriteLine($"Yes! {totalMoney - washingMachinePrice:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {washingMachinePrice - totalMoney:F2}");
+            }
+        }
+    }
+}
---
+++ 04.ForLoop_Ex/05.Salary/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _05.Salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabsCount = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < tabsCount; i++)
+            {
+                string currentTab = Console.ReadLine();
+                int fine = 0;
+
+                switch (currentTab)
+                {
+                    case "Facebook": fine = 150; break;
+                    case "Instagram": fine = 100; break;
+                    case "Reddit": fine = 50; break;
+                }
+
+                salary -= fine;
+
+                if (salary <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    break;
+                }
+            }
+
+            if (salary > 0)
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
