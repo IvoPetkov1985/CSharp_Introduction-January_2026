55b1cfef72fee3454fff07b73a1b0b3cbb9e2022
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-05 10:38:38 +0200

    Tasks 2 and 3 uploaded.

---
+++ 04.ForLoop_Ex/02.HalfSumElement/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _02.HalfSumElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numsCount = int.Parse(Console.ReadLine());
+
+            int maxNumber = int.MinValue;
+            int sum = 0;
+
+            for (int i = 0; i < numsCount; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+
+                if (currentNumber > maxNumber)
+                {
+                    maxNumber = currentNumber;
+                }
+
+                sum += currentNumber;
+            }
+
+            int sumRestNumbers = sum - maxNumber;
+
+            if (maxNumber == sumRestNumbers)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxNumber}");
+            }
+            else
+            {
+                int diff = Math.Abs(sumRestNumbers - maxNumber);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
---
+++ 04.ForLoop_Ex/03.Histogram/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _03.Histogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+
+            int firstCount = 0;
+            int secondCount = 0;
+            int thirdCount = 0;
+            int fourthCount = 0;
+            int fifthCount = 0;
+
+            for (int i = 0; i < numbersCount; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+
+                if (currentNumber < 200)
+                {
+                    firstCount++;
+                }
+                else if (currentNumber <= 399)
+                {
+                    secondCount++;
+                }
+                else if (currentNumber <= 599)
+                {
+                    thirdCount++;
+                }
+                else if (currentNumber <= 799)
+                {
+                    fourthCount++;
+                }
+                else
+                {
+                    fifthCount++;
+                }
+            }
+
+            double p1 = firstCount * 100.0 / numbersCount;
+            double p2 = secondCount * 100.0 / numbersCount;
+            double p3 = thirdCount * 100.0 / numbersCount;
+            double p4 = fourthCount * 100.0 / numbersCount;
+            double p5 = fifthCount * 100.0 / numbersCount;
+
+            Console.WriteLine($"{p1:F2}%");
+            Console.WriteLine($"{p2:F2}%");
+            Console.WriteLine($"{p3:F2}%");
+            Console.WriteLine($"{p4:F2}%");
+            Console.WriteLine($"{p5:F2}%");
+        }
+    }
+}
