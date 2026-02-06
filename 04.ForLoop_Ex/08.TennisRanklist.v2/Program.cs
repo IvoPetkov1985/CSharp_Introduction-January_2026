caca5b6dfa6ba8840e43205c06b539402ff65054
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-06 14:42:53 +0200

    Task 8 and .sln file uploaded.
    Two solutions.

---
+++ 04.ForLoop_Ex/08.TennisRanklist.v2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _08.TennisRanklist.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tournaments = int.Parse(Console.ReadLine());
+            int startingPoints = int.Parse(Console.ReadLine());
+
+            int seasonPoints = 0;
+            int wonTournaments = 0;
+
+            for (int i = 0; i < tournaments; i++)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "W":
+                        seasonPoints += 2000;
+                        wonTournaments++;
+                        break;
+                    case "F":
+                        seasonPoints += 1200;
+                        break;
+                    case "SF":
+                        seasonPoints += 720;
+                        break;
+                }
+            }
+
+            int finalPoints = startingPoints + seasonPoints;
+            int averagepoints = seasonPoints / tournaments;
+            double wonPercentage = (double)wonTournaments * 100 / tournaments;
+
+            Console.WriteLine($"Final points: {finalPoints}");
+            Console.WriteLine($"Average points: {averagepoints}");
+            Console.WriteLine($"{wonPercentage:F2}%");
+        }
+    }
+}
---
+++ 04.ForLoop_Ex/08.TennisRanklist/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _08.TennisRanklist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tournaments = int.Parse(Console.ReadLine());
+            int points = int.Parse(Console.ReadLine());
+
+            int addedPoints = 0;
+            int won = 0;
+
+            for (int i = 0; i < tournaments; i++)
+            {
+                string status = Console.ReadLine();
+
+                if (status == "W")
+                {
+                    won++;
+                    addedPoints += 2000;
+                }
+                else if (status == "F")
+                {
+                    addedPoints += 1200;
+                }
+                else if (status == "SF")
+                {
+                    addedPoints += 720;
+                }
+            }
+
+            int finalPoints = points + addedPoints;
+            double averagepoints = Math.Floor(addedPoints / (tournaments * 1.0));
+            double wonPercent = won * 100.0 / tournaments;
+
+            Console.WriteLine($"Final points: {finalPoints}");
+            Console.WriteLine($"Average points: {averagepoints}");
+            Console.WriteLine($"{wonPercent:F2}%");
+        }
+    }
+}
