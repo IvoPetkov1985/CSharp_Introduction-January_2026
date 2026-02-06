05bca53610136dda83525a8f7cfac82568bc7347
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-06 14:41:47 +0200

    Tasks 6 and 7 uploaded.

---
+++ 04.ForLoop_Ex/06.Oscars/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _06.Oscars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string actorName = Console.ReadLine();
+            double academyPoints = double.Parse(Console.ReadLine());
+            int juryCount = int.Parse(Console.ReadLine());
+
+            double neededPoints = 1250.5;
+
+            for (int i = 0; i < juryCount; i++)
+            {
+                string juryName = Console.ReadLine();
+                double juryPoints = double.Parse(Console.ReadLine());
+                double assessment = juryName.Length * juryPoints / 2;
+                academyPoints += assessment;
+
+                if (academyPoints > neededPoints)
+                {
+                    Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {academyPoints:F1}!");
+                    break;
+                }
+            }
+
+            if (academyPoints <= neededPoints)
+            {
+                Console.WriteLine($"Sorry, {actorName} you need {neededPoints - academyPoints:F1} more!");
+            }
+        }
+    }
+}
---
+++ 04.ForLoop_Ex/07.TrekkingMania/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _07.TrekkingMania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int groupsCount = int.Parse(Console.ReadLine());
+
+            int mussala = 0;
+            int montblanc = 0;
+            int kilimandjaro = 0;
+            int k2 = 0;
+            int everest = 0;
+
+            for (int i = 0; i < groupsCount; i++)
+            {
+                int groupSize = int.Parse(Console.ReadLine());
+
+                if (groupSize <= 5)
+                {
+                    mussala += groupSize;
+                }
+                else if (groupSize <= 12)
+                {
+                    montblanc += groupSize;
+                }
+                else if (groupSize <= 25)
+                {
+                    kilimandjaro += groupSize;
+                }
+                else if (groupSize <= 40)
+                {
+                    k2 += groupSize;
+                }
+                else
+                {
+                    everest += groupSize;
+                }
+            }
+
+            int allTrekkers = mussala + montblanc + kilimandjaro + k2 + everest;
+            double mussalaPercentage = mussala * 100.0 / allTrekkers;
+            double montblancPercentage = montblanc * 100.0 / allTrekkers;
+            double kilimandjaroPercentage = kilimandjaro * 100.0 / allTrekkers;
+            double k2Percentage = k2 * 100.0 / allTrekkers;
+            double everestPercentage = everest * 100.0 / allTrekkers;
+
+            Console.WriteLine($"{mussalaPercentage:F2}%");
+            Console.WriteLine($"{montblancPercentage:F2}%");
+            Console.WriteLine($"{kilimandjaroPercentage:F2}%");
+            Console.WriteLine($"{k2Percentage:F2}%");
+            Console.WriteLine($"{everestPercentage:F2}%");
+        }
+    }
+}
