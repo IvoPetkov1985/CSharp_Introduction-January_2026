a09a0d85dc54e45bde1667cc8487f279ece7a1f8
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-07 13:41:56 +0200

    Tasks 5 and 6 uploaded.

---
+++ 04.ForLoop_MoreEx/05.GameOfIntervals/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace _05.GameOfIntervals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int moves = int.Parse(Console.ReadLine());
+
+            double result = 0;
+
+            int from0To9 = 0;
+            int from10To19 = 0;
+            int from20To29 = 0;
+            int from30To39 = 0;
+            int from40To50 = 0;
+            int invalidNums = 0;
+
+            for (int i = 0; i < moves; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < 0 || number > 50)
+                {
+                    invalidNums++;
+                    result /= 2;
+                }
+                else if (number <= 9)
+                {
+                    from0To9++;
+                    result += number * 0.2;
+                }
+                else if (number <= 19)
+                {
+                    from10To19++;
+                    result += number * 0.3;
+                }
+                else if (number <= 29)
+                {
+                    from20To29++;
+                    result += number * 0.4;
+                }
+                else if (number <= 39)
+                {
+                    from30To39++;
+                    result += 50;
+                }
+                else
+                {
+                    from40To50++;
+                    result += 100;
+                }
+            }
+
+            double from0To9Percent = from0To9 * 100.0 / moves;
+            double from10To19Percent = from10To19 * 100.0 / moves;
+            double from20To29Percent = from20To29 * 100.0 / moves;
+            double from30To39Percent = from30To39 * 100.0 / moves;
+            double from40To50Percent = from40To50 * 100.0 / moves;
+            double invalidNumsPercent = invalidNums * 100.0 / moves;
+
+            Console.WriteLine($"{result:F2}");
+            Console.WriteLine($"From 0 to 9: {from0To9Percent:F2}%");
+            Console.WriteLine($"From 10 to 19: {from10To19Percent:F2}%");
+            Console.WriteLine($"From 20 to 29: {from20To29Percent:F2}%");
+            Console.WriteLine($"From 30 to 39: {from30To39Percent:F2}%");
+            Console.WriteLine($"From 40 to 50: {from40To50Percent:F2}%");
+            Console.WriteLine($"Invalid numbers: {invalidNumsPercent:F2}%");
+        }
+    }
+}
---
+++ 04.ForLoop_MoreEx/06.Bills/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _06.Bills
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int months = int.Parse(Console.ReadLine());
+
+            int waterBill = 20;
+            int internetBill = 15;
+
+            double electricity = 0;
+            double water = 0;
+            double internet = 0;
+            double others = 0;
+            double total = 0;
+
+            for (int i = 0; i < months; i++)
+            {
+                double electricityBill = double.Parse(Console.ReadLine());
+                double otherBills = (electricityBill + waterBill + internetBill) * 1.20;
+
+                electricity += electricityBill;
+                water += waterBill;
+                internet += internetBill;
+                others += otherBills;
+                total += electricityBill + waterBill + internetBill + otherBills;
+            }
+
+            double average = total / months;
+
+            Console.WriteLine($"Electricity: {electricity:F2} lv");
+            Console.WriteLine($"Water: {water:F2} lv");
+            Console.WriteLine($"Internet: {internet:F2} lv");
+            Console.WriteLine($"Other: {others:F2} lv");
+            Console.WriteLine($"Average: {average:F2} lv");
+        }
+    }
+}
