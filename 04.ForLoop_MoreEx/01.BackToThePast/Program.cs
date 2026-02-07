e9b4790720796bf440d49bf224897c85657fe292
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-07 13:40:26 +0200

    Tasks 1 and 2 uploaded.

---
+++ 04.ForLoop_MoreEx/01.BackToThePast/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _01.BackToThePast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int endYear = int.Parse(Console.ReadLine());
+
+            int startYear = 1800;
+            int age = 18;
+            double expenses = 0;
+            double expensesPerYear = 12000;
+
+            for (int year = startYear; year <= endYear; year++)
+            {
+                if (year % 2 == 0)
+                {
+                    expenses += expensesPerYear;
+                }
+                else
+                {
+                    expenses += expensesPerYear + 50 * age;
+                }
+
+                age++;
+            }
+
+            double diff = Math.Abs(money - expenses);
+
+            if (money >= expenses)
+            {
+                Console.WriteLine($"Yes! He will live a carefree life and will have {diff:F2} dollars left.");
+            }
+            else
+            {
+                Console.WriteLine($"He will need {diff:F2} dollars to survive.");
+            }
+        }
+    }
+}
---
+++ 04.ForLoop_MoreEx/02.Hospital/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _02.Hospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysCount = int.Parse(Console.ReadLine());
+
+            int doctorsCount = 7;
+            int treated = 0;
+            int untreated = 0;
+
+            for (int i = 1; i <= daysCount; i++)
+            {
+                if (i % 3 == 0 && untreated > treated)
+                {
+                    doctorsCount++;
+                }
+
+                int currentPatients = int.Parse(Console.ReadLine());
+
+                if (currentPatients > doctorsCount)
+                {
+                    treated += doctorsCount;
+                    untreated += currentPatients - doctorsCount;
+                }
+                else
+                {
+                    treated += currentPatients;
+                }
+            }
+
+            Console.WriteLine($"Treated patients: {treated}.");
+            Console.WriteLine($"Untreated patients: {untreated}.");
+        }
+    }
+}
