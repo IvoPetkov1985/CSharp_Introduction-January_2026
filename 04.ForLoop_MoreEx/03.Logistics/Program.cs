9eb5a6e6dc934706ac17dc8c21e8df5d529f4a07
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-07 13:41:25 +0200

    Tasks 3 and 4 uploaded.

---
+++ 04.ForLoop_MoreEx/03.Logistics/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _03.Logistics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            double busTonPrice = 200;
+            double truckTonPrice = 175;
+            double trainTonPrice = 120;
+
+            int busTons = 0;
+            int truckTons = 0;
+            int trainTons = 0;
+            double totalMoney = 0;
+
+            for (int i = 1; i <= count; i++)
+            {
+                int currentTons = int.Parse(Console.ReadLine());
+
+                if (currentTons <= 3)
+                {
+                    busTons += currentTons;
+                    totalMoney += currentTons * busTonPrice;
+                }
+                else if (currentTons >= 4 && currentTons <= 11)
+                {
+                    truckTons += currentTons;
+                    totalMoney += currentTons * truckTonPrice;
+                }
+                else if (currentTons >= 12)
+                {
+                    trainTons += currentTons;
+                    totalMoney += currentTons * trainTonPrice;
+                }
+            }
+
+            int totalTons = busTons + truckTons + trainTons;
+            double averagePricePerTon = totalMoney / (totalTons * 1.0);
+            double busPercentage = busTons * 100.0 / (totalTons * 1.0);
+            double truckPercentage = truckTons * 100.0 / (totalTons * 1.0);
+            double trainPercentage = trainTons * 100.0 / (totalTons * 1.0);
+
+            Console.WriteLine($"{averagePricePerTon:F2}");
+            Console.WriteLine($"{busPercentage:F2}%");
+            Console.WriteLine($"{truckPercentage:F2}%");
+            Console.WriteLine($"{trainPercentage:F2}%");
+        }
+    }
+}
---
+++ 04.ForLoop_MoreEx/04.Grades/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _04.Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int students = int.Parse(Console.ReadLine());
+
+            int aboveFive = 0;
+            int aboveFour = 0;
+            int aboveThree = 0;
+            int fail = 0;
+            double totalGrade = 0;
+
+            for (int i = 0; i < students; i++)
+            {
+                double grade = double.Parse(Console.ReadLine());
+
+                if (grade >= 5)
+                {
+                    aboveFive++;
+                }
+                else if (grade >= 4)
+                {
+                    aboveFour++;
+                }
+                else if (grade >= 3)
+                {
+                    aboveThree++;
+                }
+                else
+                {
+                    fail++;
+                }
+
+                totalGrade += grade;
+            }
+
+            double topStudentsPercent = aboveFive * 100.0 / students;
+            double aboveFourPercent = aboveFour * 100.0 / students;
+            double aboveThreePercent = aboveThree * 100.0 / students;
+            double failPercent = fail * 100.0 / students;
+            double averageGrade = totalGrade / students;
+
+            Console.WriteLine($"Top students: {topStudentsPercent:F2}%");
+            Console.WriteLine($"Between 4.00 and 4.99: {aboveFourPercent:F2}%");
+            Console.WriteLine($"Between 3.00 and 3.99: {aboveThreePercent:F2}%");
+            Console.WriteLine($"Fail: {failPercent:F2}%");
+            Console.WriteLine($"Average: {averageGrade:F2}");
+        }
+    }
+}
