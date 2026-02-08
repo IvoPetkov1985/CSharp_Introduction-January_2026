f7d537253a93e1764c456e1ba8823515d89765a3
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-08 10:50:24 +0200

    Task 8 uploaded.
    Two solutions.

---
+++ 04.ForLoop_MoreEx/08.EqualPairs.v2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _08.EqualPairs.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pairs = int.Parse(Console.ReadLine());
+            int[] values = new int[pairs];
+
+            for (int i = 0; i < pairs; i++)
+            {
+                int firstNum = int.Parse(Console.ReadLine());
+                int secondNum = int.Parse(Console.ReadLine());
+                int value = firstNum + secondNum;
+                values[i] = value;
+            }
+
+            if (values.All(x => x == values.First()))
+            {
+                Console.WriteLine($"Yes, value={values.First()}");
+            }
+            else
+            {
+                int maxDiff = 0;
+                int currentNum = values.First();
+
+                for (int i = 1; i < values.Length; i++)
+                {
+                    int nextNum = values[i];
+                    int currentDiff = Math.Abs(nextNum - currentNum);
+
+                    if (currentDiff > maxDiff)
+                    {
+                        maxDiff = currentDiff;
+                    }
+
+                    currentNum = nextNum;
+                }
+
+                Console.WriteLine($"No, maxdiff={maxDiff}");
+            }
+        }
+    }
+}
---
+++ 04.ForLoop_MoreEx/08.EqualPairs/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _08.EqualPairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pairsCount = int.Parse(Console.ReadLine());
+
+            bool areEqual = true;
+            int firstPairFirstNum = int.Parse(Console.ReadLine());
+            int firstPairSecondNum = int.Parse(Console.ReadLine());
+            int firstValue = firstPairFirstNum + firstPairSecondNum;
+            int lastValue = firstValue;
+            int maxDiff = 0;
+
+            for (int i = 1; i < pairsCount; i++)
+            {
+                int firstNum = int.Parse(Console.ReadLine());
+                int secondNum = int.Parse(Console.ReadLine());
+                int value = firstNum + secondNum;
+
+                if (value != lastValue)
+                {
+                    int currentDiff = Math.Abs(value - lastValue);
+                    areEqual = false;
+                    lastValue = value;
+
+                    if (currentDiff > maxDiff)
+                    {
+                        maxDiff = currentDiff;
+                    }
+                }
+            }
+
+            if (areEqual)
+            {
+                Console.WriteLine($"Yes, value={lastValue}");
+            }
+            else
+            {
+                Console.WriteLine($"No, maxdiff={maxDiff}");
+            }
+        }
+    }
+}
