646af36a6ad80a9ce0ee5f9868f8d0308652e4fb
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-08 10:51:06 +0200

    Tasks 9 and 10 uploaded.
    Nested for loops.

---
+++ 04.ForLoop_MoreEx/10.Clock_Part2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _10.Clock_Part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int hours = 0; hours < 24; hours++)
+            {
+                for (int minutes = 0; minutes < 60; minutes++)
+                {
+                    for (int seconds = 0; seconds < 60; seconds++)
+                    {
+                        Console.WriteLine($"{hours} : {minutes} : {seconds}");
+                    }
+                }
+            }
+        }
+    }
+}
