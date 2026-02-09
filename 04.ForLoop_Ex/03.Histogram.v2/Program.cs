374cfed3193791b31574aaa2a3dc5ab8f603fbf2
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-09 14:28:47 +0200

    Tasks 3 and 7 uploaded.
    Fancy solutions with List<int>.

---
+++ 04.ForLoop_Ex/03.Histogram.v2/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _03.Histogram.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+
+            List<int> p1Numbers = new();
+            List<int> p2Numbers = new();
+            List<int> p3Numbers = new();
+            List<int> p4Numbers = new();
+            List<int> p5Numbers = new();
+
+            for (int i = 0; i < numbersCount; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+
+                if (currentNumber < 200)
+                {
+                    p1Numbers.Add(currentNumber);
+                }
+                else if (currentNumber < 400)
+                {
+                    p2Numbers.Add(currentNumber);
+                }
+                else if (currentNumber < 600)
+                {
+                    p3Numbers.Add(currentNumber);
+                }
+                else if (currentNumber < 800)
+                {
+                    p4Numbers.Add(currentNumber);
+                }
+                else
+                {
+                    p5Numbers.Add(currentNumber);
+                }
+            }
+
+            double p1Percent = p1Numbers.Count * 100.0 / numbersCount;
+            double p2Percent = p2Numbers.Count * 100.0 / numbersCount;
+            double p3Percent = p3Numbers.Count * 100.0 / numbersCount;
+            double p4Percent = p4Numbers.Count * 100.0 / numbersCount;
+            double p5Percent = p5Numbers.Count * 100.0 / numbersCount;
+
+            Console.WriteLine($"{p1Percent:F2}%");
+            Console.WriteLine($"{p2Percent:F2}%");
+            Console.WriteLine($"{p3Percent:F2}%");
+            Console.WriteLine($"{p4Percent:F2}%");
+            Console.WriteLine($"{p5Percent:F2}%");
+        }
+    }
+}
---
+++ 04.ForLoop_Ex/07.TrekkingMania.v2/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _07.TrekkingMania.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int groups = int.Parse(Console.ReadLine());
+
+            List<int> mussala = new();
+            List<int> montblanc = new();
+            List<int> kilimangjaro = new();
+            List<int> k2 = new();
+            List<int> everest = new();
+
+            for (int i = 0; i < groups; i++)
+            {
+                int groupMembers = int.Parse(Console.ReadLine());
+
+                if (groupMembers <= 5)
+                {
+                    mussala.Add(groupMembers);
+                }
+                else if (groupMembers <= 12)
+                {
+                    montblanc.Add(groupMembers);
+                }
+                else if (groupMembers <= 25)
+                {
+                    kilimangjaro.Add(groupMembers);
+                }
+                else if (groupMembers <= 40)
+                {
+                    k2.Add(groupMembers);
+                }
+                else
+                {
+                    everest.Add(groupMembers);
+                }
+            }
+
+            int totalMembers = mussala.Sum() + montblanc.Sum() + kilimangjaro.Sum() + k2.Sum() + everest.Sum();
+            double mussalaPercent = mussala.Sum() * 100.0 / totalMembers;
+            double montblancPercent = montblanc.Sum() * 100.0 / totalMembers;
+            double kilimandjaroPercent = kilimangjaro.Sum() * 100.0 / totalMembers;
+            double k2Percent = k2.Sum() * 100.0 / totalMembers;
+            double everestPercent = everest.Sum() * 100.0 / totalMembers;
+
+            Console.WriteLine($"{mussalaPercent:F2}%");
+            Console.WriteLine($"{montblancPercent:F2}%");
+            Console.WriteLine($"{kilimandjaroPercent:F2}%");
+            Console.WriteLine($"{k2Percent:F2}%");
+            Console.WriteLine($"{everestPercent:F2}%");
+        }
+    }
+}
