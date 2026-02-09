91de3f36a63fee5e377d69caa9ee7e1047933cc5
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-09 13:41:00 +0200

    Task 2 uploaded.
    Resolved with another solution.

---
+++ 04.ForLoop_Ex/02.HalfSumElement.v2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _02.HalfSumElement.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[numbersCount];
+
+            for (int i = 0; i < numbersCount; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                numbers[i] = currentNumber;
+            }
+
+            int maxNum = numbers.Max();
+            int totalSum = numbers.Sum();
+
+            if (maxNum == totalSum - maxNum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + maxNum);
+            }
+            else
+            {
+                int diff = Math.Abs(maxNum - (totalSum - maxNum));
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + diff);
+            }
+        }
+    }
+}
