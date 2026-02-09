83dbe26ea08e18cfdd42d6f7e0063f0c0c6ed91a
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-09 13:37:56 +0200

    Tasks 9 and 10 uploaded.
    Advanced solutions.

---
+++ 04.ForLoop_Lab/09.LeftAndRightSum.v2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _09.LeftAndRightSum.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[count * 2];
+
+            for (int i = 0; i < count * 2; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                numbers[i] = currentNum;
+            }
+
+            int leftSum = numbers
+                .Skip(0)
+                .Take(count)
+                .Sum();
+            int rightSum = numbers
+                .Skip(count)
+                .Take(count)
+                .Sum();
+
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine("Yes, sum = " + leftSum);
+            }
+            else
+            {
+                int diff = Math.Abs(leftSum - rightSum);
+                Console.WriteLine("No, diff = " + diff);
+            }
+        }
+    }
+}
---
+++ 04.ForLoop_Lab/10.OddEvenSum.v2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _10.OddEvenSum.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            List<int> evenPositionNums = new();
+            List<int> oddPositionNums = new();
+
+            for (int i = 0; i < count; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+
+                if (i % 2 == 0)
+                {
+                    evenPositionNums.Add(currentNum);
+                }
+                else
+                {
+                    oddPositionNums.Add(currentNum);
+                }
+            }
+
+            int evenSum = evenPositionNums.Sum();
+            int oddSum = oddPositionNums.Sum();
+
+            if (evenSum == oddSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + evenSum);
+            }
+            else
+            {
+                int diff = Math.Abs(evenSum - oddSum);
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + diff);
+            }
+        }
+    }
+}
