2b37ed3001476c5c321f7f45af115d2636cb1a12
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-09 13:35:54 +0200

    Tasks 7 and 8 uploaded.
    Advanced solutions.

---
+++ 04.ForLoop_Lab/07.SumNumbers.v2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _07.SumNumbers.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                numbers[i] = currentNum;
+            }
+
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
---
+++ 04.ForLoop_Lab/08.NumberSequence.v2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _08.NumberSequence.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            int[] ints = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                ints[i] = currentNum;
+            }
+
+            int maxNumber = ints.Max();
+            int minNumber = ints.Min();
+
+            Console.WriteLine("Max number: " + maxNumber);
+            Console.WriteLine("Min number: " + minNumber);
+        }
+    }
+}
