5a86ccbac2575441908dd6066dfca0dfbdc9b57e
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-10 12:00:14 +0200

    Tasks 1 and 2 uploaded.
    Task 2 with two solutions.

---
+++ 05.WhileLoop_Lab/02.Password.v2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _02.Password.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+            string attemptPass = Console.ReadLine();
+
+            while (attemptPass != password)
+            {
+                attemptPass = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Welcome {username}!");
+        }
+    }
+}
---
+++ 05.WhileLoop_Lab/02.Password/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _02.Password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+            string inputPass = Console.ReadLine();
+
+            while (true)
+            {
+                if (inputPass == password)
+                {
+                    Console.WriteLine($"Welcome {username}!");
+                    break;
+                }
+
+                inputPass = Console.ReadLine();
+            }
+        }
+    }
+}
