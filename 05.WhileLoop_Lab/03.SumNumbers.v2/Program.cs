708ec412bc940aa66ac3144f70754027703ab697
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-10 12:03:08 +0200

    Task 3 uploaded.
    Two solutions.

---
+++ 05.WhileLoop_Lab/03.SumNumbers.v2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _03.SumNumbers.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int upperBound = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (true)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+
+                if (sum >= upperBound)
+                {
+                    Console.WriteLine(sum);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ 05.WhileLoop_Lab/03.SumNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _03.SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            while (sum < limit)
+            {
+                int inputNum = int.Parse(Console.ReadLine());
+                sum += inputNum;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
