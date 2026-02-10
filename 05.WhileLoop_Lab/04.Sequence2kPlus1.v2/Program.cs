4297d22423b605cdfaca8f4166bf6ac5b3d20e44
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-10 12:04:00 +0200

    Task 4 and .sln file uploaded.
    Two solutions of task 4.

---
+++ 05.WhileLoop_Lab/04.Sequence2kPlus1.v2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _04.Sequence2kPlus1.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            int k = 1;
+
+            while (true)
+            {
+                Console.WriteLine(k);
+                k = k * 2 + 1;
+
+                if (k > inputNumber)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
