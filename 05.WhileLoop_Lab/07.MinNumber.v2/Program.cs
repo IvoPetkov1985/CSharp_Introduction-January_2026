7f21e2b3eb321d284c130fab2cd2647cdae2f455
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-11 18:09:58 +0200

    Task 7 uploaded.

---
+++ 05.WhileLoop_Lab/07.MinNumber.v2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _07.MinNumber.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            int minNumber = int.MaxValue;
+
+            while (command != "Stop")
+            {
+                int currentNumber = int.Parse(command);
+
+                if (currentNumber < minNumber)
+                {
+                    minNumber = currentNumber;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(minNumber);
+        }
+    }
+}
---
+++ 05.WhileLoop_Lab/07.MinNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _07.MinNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            List<int> numbers = new();
+
+            while (true)
+            {
+                if (command == "Stop")
+                {
+                    break;
+                }
+
+                int currentNumber = int.Parse(command);
+                numbers.Add(currentNumber);
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(numbers.Min());
+        }
+    }
+}
