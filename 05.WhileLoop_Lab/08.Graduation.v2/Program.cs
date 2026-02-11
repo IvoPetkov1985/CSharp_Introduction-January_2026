fe5797c05b0a3b8edded9ae6f010dfee0389c2a7
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-11 18:10:31 +0200

    Last task uploaded.

---
+++ 05.WhileLoop_Lab/08.Graduation.v2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _08.Graduation.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            int failed = 0;
+            List<double> marks = new();
+
+            while (marks.Count < 12)
+            {
+                double yearMark = double.Parse(Console.ReadLine());
+
+                if (yearMark >= 4.00)
+                {
+                    marks.Add(yearMark);
+                }
+                else
+                {
+                    failed++;
+
+                    if (failed > 1)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (marks.Count == 12)
+            {
+                Console.WriteLine($"{name} graduated. Average grade: {marks.Average():F2}");
+            }
+            else
+            {
+                Console.WriteLine($"{name} has been excluded at {marks.Count + 1} grade");
+            }
+        }
+    }
+}
---
+++ 05.WhileLoop_Lab/08.Graduation/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _08.Graduation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName = Console.ReadLine();
+
+            double sumGrades = 0;
+            int studyClass = 1;
+            int failedTimes = 0;
+            bool graduated = false;
+
+            while (true)
+            {
+                double yearGrade = double.Parse(Console.ReadLine());
+
+                if (yearGrade >= 4)
+                {
+                    studyClass++;
+                    sumGrades += yearGrade;
+                }
+                else
+                {
+                    failedTimes++;
+                }
+
+                if (failedTimes > 1)
+                {
+                    break;
+                }
+
+                if (studyClass > 12)
+                {
+                    graduated = true;
+                    break;
+                }
+            }
+
+            if (graduated)
+            {
+                double averageGrade = sumGrades / 12;
+                Console.WriteLine($"{studentName} graduated. Average grade: {averageGrade:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"{studentName} has been excluded at {studyClass} grade");
+            }
+        }
+    }
+}
