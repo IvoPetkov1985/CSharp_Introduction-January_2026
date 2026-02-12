95804eedbd948b6579b24bc43b89c56a1ef336f0
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-12 14:44:16 +0200

    Task 3 uploaded.
    Two solutions.

---
+++ 05.WhileLoop_Ex/03.Vacation.v2/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _03.Vacation.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double tripPrice = double.Parse(Console.ReadLine());
+            double ownedMoney = double.Parse(Console.ReadLine());
+
+            int daysCount = 0;
+            int spendDaysInRow = 0;
+
+            while (spendDaysInRow < 5 && ownedMoney < tripPrice)
+            {
+                string action = Console.ReadLine();
+                double amount = double.Parse(Console.ReadLine());
+                daysCount++;
+
+                switch (action)
+                {
+                    case "spend":
+                        spendDaysInRow++;
+                        ownedMoney -= amount;
+
+                        if (ownedMoney < 0)
+                        {
+                            ownedMoney = 0;
+                        }
+
+                        break;
+
+                    case "save":
+                        spendDaysInRow = 0;
+                        ownedMoney += amount;
+                        break;
+                }
+            }
+
+            if (spendDaysInRow == 5)
+            {
+                Console.WriteLine("You can't save the money.");
+                Console.WriteLine(daysCount);
+            }
+
+            if (ownedMoney >= tripPrice)
+            {
+                Console.WriteLine($"You saved the money for {daysCount} days.");
+            }
+        }
+    }
+}
---
+++ 05.WhileLoop_Ex/03.Vacation/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace _03.Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double tripPrice = double.Parse(Console.ReadLine());
+            double realMoney = double.Parse(Console.ReadLine());
+
+            int totalDays = 0;
+            int spendingDays = 0;
+            bool succeeded = true;
+
+            while (true)
+            {
+                string action = Console.ReadLine();
+                double amount = double.Parse(Console.ReadLine());
+
+                totalDays++;
+
+                if (action == "spend")
+                {
+                    spendingDays++;
+                    realMoney -= amount;
+
+                    if (realMoney < 0)
+                    {
+                        realMoney = 0;
+                    }
+
+                    if (spendingDays == 5)
+                    {
+                        succeeded = false;
+                        break;
+                    }
+                }
+                else if (action == "save")
+                {
+                    spendingDays = 0;
+                    realMoney += amount;
+
+                    if (realMoney >= tripPrice)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (succeeded == false)
+            {
+                Console.WriteLine("You can't save the money.");
+                Console.WriteLine(totalDays);
+            }
+            else
+            {
+                Console.WriteLine($"You saved the money for {totalDays} days.");
+            }
+        }
+    }
+}
