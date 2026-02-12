36a7cd5439bb197a3c8c9004451744ba04a1dbba
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-12 14:45:09 +0200

    Task 4 uploaded and .sln added.
    Two solutions of task 4.

---
+++ 05.WhileLoop_Ex/04.Walking.v2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _04.Walking.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int goal = 10000;
+
+            int currentResult = 0;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Going home")
+                {
+                    int addedSteps = int.Parse(Console.ReadLine());
+                    currentResult += addedSteps;
+                    break;
+                }
+
+                int steps = int.Parse(command);
+                currentResult += steps;
+
+                if (currentResult >= goal)
+                {
+                    break;
+                }
+            }
+
+            if (currentResult >= goal)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{currentResult - goal} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{goal - currentResult} more steps to reach goal.");
+            }
+        }
+    }
+}
---
+++ 05.WhileLoop_Ex/04.Walking/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _04.Walking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int goal = 10000;
+            int totalSteps = 0;
+            string command = Console.ReadLine();
+
+            while (command != "Going home")
+            {
+                int steps = int.Parse(command);
+                totalSteps += steps;
+
+                if (totalSteps >= goal)
+                {
+                    break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (command == "Going home")
+            {
+                int addedSteps = int.Parse(Console.ReadLine());
+                totalSteps += addedSteps;
+            }
+
+            if (totalSteps >= goal)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{totalSteps - goal} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{goal - totalSteps} more steps to reach goal.");
+            }
+        }
+    }
+}
