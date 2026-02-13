e3ca54b000e396224a18d67073007ad30383fe94
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-13 12:38:21 +0200

    Task 5 uploaded.
    Two different solutions.

---
+++ 05.WhileLoop_Ex/05.Coins.v2/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _05.Coins.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = double.Parse(Console.ReadLine());
+            int sumInCents = (int)(sum * 100);
+            int coinsReturned = 0;
+
+            while (true)
+            {
+                if (sumInCents >= 200)
+                {
+                    sumInCents -= 200;
+                }
+                else if (sumInCents >= 100)
+                {
+                    sumInCents -= 100;
+                }
+                else if (sumInCents >= 50)
+                {
+                    sumInCents -= 50;
+                }
+                else if (sumInCents >= 20)
+                {
+                    sumInCents -= 20;
+                }
+                else if (sumInCents >= 10)
+                {
+                    sumInCents -= 10;
+                }
+                else if (sumInCents >= 5)
+                {
+                    sumInCents -= 5;
+                }
+                else if (sumInCents >= 2)
+                {
+                    sumInCents -= 2;
+                }
+                else if (sumInCents >= 1)
+                {
+                    sumInCents -= 1;
+                }
+
+                coinsReturned++;
+
+                if (sumInCents == 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(coinsReturned);
+        }
+    }
+}
---
+++ 05.WhileLoop_Ex/05.Coins/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _05.Coins
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+
+            int moneyInCents = (int)(money * 100);
+            int coins = 0;
+
+            while (moneyInCents > 0)
+            {
+                if (moneyInCents >= 200)
+                {
+                    moneyInCents -= 200;
+                }
+                else if (moneyInCents >= 100)
+                {
+                    moneyInCents -= 100;
+                }
+                else if (moneyInCents >= 50)
+                {
+                    moneyInCents -= 50;
+                }
+                else if (moneyInCents >= 20)
+                {
+                    moneyInCents -= 20;
+                }
+                else if (moneyInCents >= 10)
+                {
+                    moneyInCents -= 10;
+                }
+                else if (moneyInCents >= 5)
+                {
+                    moneyInCents -= 5;
+                }
+                else if (moneyInCents >= 2)
+                {
+                    moneyInCents -= 2;
+                }
+                else
+                {
+                    moneyInCents -= 1;
+                }
+
+                coins++;
+            }
+
+            Console.WriteLine(coins);
+        }
+    }
+}
