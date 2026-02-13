2c116f66f29bb6cd6c331adb3eea03d24ba59a54
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-13 12:39:21 +0200

    Task 6 uploaded.
    Two different solutions.

---
+++ 05.WhileLoop_Ex/06.Cake.v2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _06.Cake.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+
+            int cakePieces = width * length;
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "STOP")
+            {
+                int pieces = int.Parse(input);
+                cakePieces -= pieces;
+
+                if (cakePieces < 0)
+                {
+                    Console.WriteLine($"No more cake left! You need {cakePieces * -1} pieces more.");
+                    break;
+                }
+            }
+
+            if (input == "STOP")
+            {
+                Console.WriteLine($"{cakePieces} pieces are left.");
+            }
+        }
+    }
+}
---
+++ 05.WhileLoop_Ex/06.Cake/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _06.Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cakeWidth = int.Parse(Console.ReadLine());
+            int cakeLength = int.Parse(Console.ReadLine());
+
+            int totalPieces = cakeWidth * cakeLength;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "STOP")
+                {
+                    Console.WriteLine($"{totalPieces} pieces are left.");
+                    break;
+                }
+
+                int pieces = int.Parse(command);
+                totalPieces -= pieces;
+
+                if (totalPieces < 0)
+                {
+                    Console.WriteLine($"No more cake left! You need {Math.Abs(totalPieces)} pieces more.");
+                    break;
+                }
+            }
+        }
+    }
+}
