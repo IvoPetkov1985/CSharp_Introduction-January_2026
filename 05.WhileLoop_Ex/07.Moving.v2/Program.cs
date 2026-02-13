0691d906463adfdeae3bed94a85c0fb48b07bc94
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-13 12:40:15 +0200

    Task 7 and .sln file uploaded.
    Two different solutions.

---
+++ 05.WhileLoop_Ex/07.Moving.v2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _07.Moving.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int spaceWidth = int.Parse(Console.ReadLine());
+            int spaceLength = int.Parse(Console.ReadLine());
+            int spaceHeight = int.Parse(Console.ReadLine());
+
+            int freeSpace = spaceWidth * spaceLength * spaceHeight;
+            bool isFine = true;
+            string command = Console.ReadLine();
+
+            while (command != "Done")
+            {
+                int currentSpace = int.Parse(command);
+                freeSpace -= currentSpace;
+
+                if (freeSpace < 0)
+                {
+                    Console.WriteLine($"No more free space! You need {freeSpace * -1} Cubic meters more.");
+                    isFine = false;
+                    break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (isFine == true)
+            {
+                Console.WriteLine($"{freeSpace} Cubic meters left.");
+            }
+        }
+    }
+}
---
+++ 05.WhileLoop_Ex/07.Moving/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _07.Moving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            int freeSpace = width * length * height;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Done")
+                {
+                    Console.WriteLine($"{freeSpace} Cubic meters left.");
+                    break;
+                }
+
+                int currentSpace = int.Parse(command);
+                freeSpace -= currentSpace;
+
+                if (freeSpace < 0)
+                {
+                    Console.WriteLine($"No more free space! You need {Math.Abs(freeSpace)} Cubic meters more.");
+                    break;
+                }
+            }
+        }
+    }
+}
