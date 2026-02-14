f233bfb7886a031f176c0a0654abf0fd6d903506
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-14 11:14:07 +0200

    Task 1 uploaded.
    Two solutions.

---
+++ 05.WhileLoop_MoreEx/01.Dishwasher.v2/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace _01.Dishwasher.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bottles = int.Parse(Console.ReadLine());
+
+            int detergentInMlPerBottle = 750;
+            int neededPerDish = 5;
+            int neededPerPot = 15;
+
+            int dishesWashed = 0;
+            int potsWashed = 0;
+            int counter = 0;
+            int totalDetergent = bottles * detergentInMlPerBottle;
+            bool isDetergentEnough = true;
+
+            string command = Console.ReadLine();
+
+            while (true)
+            {
+                if (command == "End")
+                {
+                    break;
+                }
+
+                int containers = int.Parse(command);
+                counter++;
+                int neededDetergent = 0;
+
+                if (counter % 3 != 0)
+                {
+                    neededDetergent = containers * neededPerDish;
+                    dishesWashed += containers;
+                    totalDetergent -= neededDetergent;
+                }
+                else
+                {
+                    neededDetergent = containers * neededPerPot;
+                    potsWashed += containers;
+                    totalDetergent -= neededDetergent;
+                }
+
+                if (totalDetergent < 0)
+                {
+                    isDetergentEnough = false;
+                    break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (isDetergentEnough == true)
+            {
+                Console.WriteLine("Detergent was enough!");
+                Console.WriteLine($"{dishesWashed} dishes and {potsWashed} pots were washed.");
+                Console.WriteLine($"Leftover detergent {totalDetergent} ml.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough detergent, {totalDetergent * -1} ml. more necessary!");
+            }
+        }
+    }
+}
---
+++ 05.WhileLoop_MoreEx/01.Dishwasher/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace _01.Dishwasher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bottles = int.Parse(Console.ReadLine());
+
+            int detergentInMl = 750 * bottles;
+            int counter = 0;
+            int dishesWashed = 0;
+            int potsWashed = 0;
+            bool isEnough = true;
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                int inputNumber = int.Parse(command);
+                counter++;
+                int detergentToUse = 0;
+
+                if (counter % 3 == 0)
+                {
+                    detergentToUse = 15 * inputNumber;
+                }
+                else
+                {
+                    detergentToUse = 5 * inputNumber;
+                }
+
+                if (detergentInMl >= detergentToUse && counter % 3 == 0)
+                {
+                    potsWashed += inputNumber;
+                    detergentInMl -= detergentToUse;                    
+                }
+                else if (detergentInMl >= detergentToUse && counter % 3 != 0)
+                {
+                    dishesWashed += inputNumber;
+                    detergentInMl -= detergentToUse;
+                }
+                else if (detergentInMl < detergentToUse)
+                {
+                    Console.WriteLine($"Not enough detergent, {detergentToUse - detergentInMl} ml. more necessary!");
+                    isEnough = false;
+                    break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (isEnough)
+            {
+                Console.WriteLine("Detergent was enough!");
+                Console.WriteLine($"{dishesWashed} dishes and {potsWashed} pots were washed.");
+                Console.WriteLine($"Leftover detergent {detergentInMl} ml.");
+            }
+        }
+    }
+}
