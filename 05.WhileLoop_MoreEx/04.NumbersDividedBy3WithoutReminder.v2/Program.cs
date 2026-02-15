8bd294f465beed8be8dc298bdea6e3f1d7e6fb71
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-15 13:36:47 +0200

    Task 4 uploaded.
    Two solutions.

---
+++ 05.WhileLoop_MoreEx/04.NumbersDividedBy3WithoutReminder.v2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _04.NumbersDividedBy3WithoutReminder.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 3; i < 100; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ 05.WhileLoop_MoreEx/04.NumbersDividedBy3WithoutReminder/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _04.NumbersDividedBy3WithoutReminder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 3;
+
+            while (num < 100)
+            {
+                Console.WriteLine(num);
+                num += 3;
+            }
+        }
+    }
+}
