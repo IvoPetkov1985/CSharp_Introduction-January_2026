e4b52eda30317c1730b7cf7e29d19b5e7802af33
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-15 13:37:32 +0200

    Task 5 uploaded.
    Two solutions.

---
+++ 05.WhileLoop_MoreEx/05.AverageNumber.v2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _05.AverageNumber.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                sum += int.Parse(Console.ReadLine());
+            }
+
+            double averageValue = (double)sum / count;
+            Console.WriteLine($"{averageValue:F2}");
+        }
+    }
+}
---
+++ 05.WhileLoop_MoreEx/05.AverageNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _05.AverageNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[numbersCount];
+
+            for (int i = 0; i < numbersCount; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                numbers[i] = number;
+            }
+
+            double averageValue = numbers.Average();
+            Console.WriteLine($"{averageValue:F2}");
+        }
+    }
+}
