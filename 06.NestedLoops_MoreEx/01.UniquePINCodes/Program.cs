a278f813776e0a43995621c17bf6654a6aea0cb2
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-16 18:18:12 +0200

    Tasks 1 and 2 uploaded.

---
+++ 06.NestedLoops_MoreEx/01.UniquePINCodes/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _01.UniquePINCodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumLimit = int.Parse(Console.ReadLine());
+            int secondNumLimit = int.Parse(Console.ReadLine());
+            int thirdNumLimit = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= firstNumLimit; i += 2)
+            {
+                for (int j = 2; j <= secondNumLimit; j++)
+                {
+                    bool isPrime = true;
+
+                    for (int p = 2; p < j; p++)
+                    {
+                        if (j % p == 0)
+                        {
+                            isPrime = false;
+                        }
+                    }
+
+                    if (isPrime == true)
+                    {
+                        for (int k = 2; k <= thirdNumLimit; k += 2)
+                        {
+                            Console.WriteLine($"{i} {j} {k}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 06.NestedLoops_MoreEx/02.LettersCombinations/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _02.LettersCombinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char startLetter = char.Parse(Console.ReadLine());
+            char endLetter = char.Parse(Console.ReadLine());
+            char skipLetter = char.Parse(Console.ReadLine());
+
+            List<string> combinations = new();
+
+            for (char i = startLetter; i <= endLetter; i++)
+            {
+                for (char j = startLetter; j <= endLetter; j++)
+                {
+                    for (char k = startLetter; k <= endLetter; k++)
+                    {
+                        if (i != skipLetter && j != skipLetter && k != skipLetter)
+                        {
+                            string currentCombination = $"{i}{j}{k} ";
+                            combinations.Add(currentCombination);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join("", combinations) + combinations.Count);
+        }
+    }
+}
