7c6e887e93d3cee3c6be57833a218bc202d73143
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-16 18:19:18 +0200

    Tasks 3 and 4 uploaded.

---
+++ 06.NestedLoops_MoreEx/03.LuckyNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _03.LuckyNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            List<string> luckyNumbers = new();
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    if (inputNumber % (i + j) == 0)
+                    {
+                        for (int k = 1; k <= 9; k++)
+                        {
+                            for (int l = 1; l <= 9; l++)
+                            {
+                                if ((i + j) == (k + l))
+                                {
+                                    string currentNumber = $"{i}{j}{k}{l}";
+                                    luckyNumbers.Add(currentNumber);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", luckyNumbers));
+        }
+    }
+}
---
+++ 06.NestedLoops_MoreEx/04.CarNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _04.CarNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intervalStart = int.Parse(Console.ReadLine());
+            int intervalEnd = int.Parse(Console.ReadLine());
+
+            List<string> carNumbers = new();
+
+            for (int i = intervalStart; i <= intervalEnd; i++)
+            {
+                for (int j = intervalStart; j <= intervalEnd; j++)
+                {
+                    for (int k = intervalStart; k <= intervalEnd; k++)
+                    {
+                        for (int l = intervalStart; l <= intervalEnd; l++)
+                        {
+                            if (((i % 2 == 0 && l % 2 != 0) || (i % 2 != 0 && l % 2 == 0)) &&
+                                (i > l) && ((j + k) % 2 == 0))
+                            {
+                                string currentNumber = $"{i}{j}{k}{l}";
+                                carNumbers.Add(currentNumber);
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", carNumbers));
+        }
+    }
+}
