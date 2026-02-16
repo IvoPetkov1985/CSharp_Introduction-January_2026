ed237bcd65b346f4797a7b613885037120bc7db7
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-16 18:19:57 +0200

    Task 5 uploaded.
    Two solutions.

---
+++ 06.NestedLoops_MoreEx/05.ChallengeTheWedding.v2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _05.ChallengeTheWedding.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int male = int.Parse(Console.ReadLine());
+            int female = int.Parse(Console.ReadLine());
+            int tablesCount = int.Parse(Console.ReadLine());
+
+            List<string> tables = new();
+
+            for (int i = 1; i <= male; i++)
+            {
+                bool areAllTabledReserved = false;
+
+                for (int j = 1; j <= female; j++)
+                {
+                    string table = $"({i} <-> {j})";
+                    tables.Add(table);
+
+                    if (tables.Count == tablesCount)
+                    {
+                        areAllTabledReserved = true;
+                        break;
+                    }
+                }
+
+                if (areAllTabledReserved)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", tables));
+        }
+    }
+}
---
+++ 06.NestedLoops_MoreEx/05.ChallengeTheWedding/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _05.ChallengeTheWedding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int men = int.Parse(Console.ReadLine());
+            int women = int.Parse(Console.ReadLine());
+            int tables = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            for (int i = 1; i <= men; i++)
+            {
+                bool areAllTablesReserved = false;
+
+                for (int j = 1; j <= women; j++)
+                {
+                    if (counter == tables)
+                    {
+                        areAllTablesReserved = true;
+                        break;
+                    }
+
+                    counter++;
+                    Console.Write($"({i} <-> {j}) ");
+                }
+
+                if (areAllTablesReserved)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
