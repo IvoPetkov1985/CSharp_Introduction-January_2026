5136954a3aeaeb038e7f9fee05a9d41989b932b4
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-17 18:40:44 +0200

    Tasks 1 and 2 uploaded.

---
+++ 06.NestedLoops_Lab/02.MultiplicationTable/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _02.MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x1 = 1; x1 <= 10; x1++)
+            {
+                for (int x2 = 1; x2 <= 10; x2++)
+                {
+                    int product = x1 * x2;
+                    Console.WriteLine($"{x1} * {x2} = {product}");
+                }
+            }
+        }
+    }
+}
