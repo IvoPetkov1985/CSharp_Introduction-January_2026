54702a4f940374d70cbf107ea967a5fb079081f1
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-17 18:42:21 +0200

    Task 4 and .sln file uploaded.

---
+++ 06.NestedLoops_Lab/04.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _04.SumOfTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intervalStart = int.Parse(Console.ReadLine());
+            int intervalEnd = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+            bool isCombinationFound = false;
+
+            for (int x1 = intervalStart; x1 <= intervalEnd; x1++)
+            {
+                for (int x2 = intervalStart; x2 <= intervalEnd; x2++)
+                {
+                    counter++;
+
+                    if (x1 + x2 == magicNumber)
+                    {
+                        isCombinationFound = true;
+                        Console.WriteLine($"Combination N:{counter} ({x1} + {x2} = {magicNumber})");
+                        break;
+                    }
+                }
+
+                if (isCombinationFound == true)
+                {
+                    break;
+                }
+            }
+
+            if (isCombinationFound == false)
+            {
+                Console.WriteLine($"{counter} combinations - neither equals {magicNumber}");
+            }
+        }
+    }
+}
