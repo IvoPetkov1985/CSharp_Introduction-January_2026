566deadd76070e2356b61594017eae795d6bd877
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-17 18:41:40 +0200

    Task 3 uploaded.

---
+++ 06.NestedLoops_Lab/03.Combinations/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _03.Combinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            for (int x1 = 0; x1 <= inputNumber; x1++)
+            {
+                for (int x2 = 0; x2 <= inputNumber; x2++)
+                {
+                    for (int x3 = 0; x3 <= inputNumber; x3++)
+                    {
+                        if (x1 + x2 + x3 == inputNumber)
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
