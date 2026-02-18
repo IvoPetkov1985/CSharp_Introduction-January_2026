adca618165cc82aaf332f209109da4c5d3bfc300
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-18 12:33:12 +0200

    Task 5 uploaded.
    Two solutions.

---
+++ 06.NestedLoops_Lab/05.Travelling.v2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _05.Travelling.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+
+            while (destination != "End")
+            {
+                double neededMoney = double.Parse(Console.ReadLine());
+                double savedMoney = 0;
+
+                while (savedMoney < neededMoney)
+                {
+                    double currentSaving = double.Parse(Console.ReadLine());
+                    savedMoney += currentSaving;
+                }
+
+                Console.WriteLine($"Going to {destination}!");
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ 06.NestedLoops_Lab/05.Travelling/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _05.Travelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string destination = Console.ReadLine();
+
+                if (destination == "End")
+                {
+                    break;
+                }
+
+                double neededMoney = double.Parse(Console.ReadLine());
+                double savedMoney = 0;
+
+                while (true)
+                {
+                    double currentSum = double.Parse(Console.ReadLine());
+                    savedMoney += currentSum;
+
+                    if (savedMoney >= neededMoney)
+                    {
+                        Console.WriteLine($"Going to {destination}!");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
