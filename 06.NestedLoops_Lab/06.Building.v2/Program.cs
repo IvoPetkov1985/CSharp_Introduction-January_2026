aad66c93dec33d1b209eb30de669227b76848780
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-18 12:34:11 +0200

    Task 6 uploaded.
    Two solutions.

---
+++ 06.NestedLoops_Lab/06.Building.v2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _06.Building.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int floorsCount = int.Parse(Console.ReadLine());
+            int roomsCount = int.Parse(Console.ReadLine());
+
+            for (int floor = floorsCount; floor >= 1; floor--)
+            {
+                string outputRow = string.Empty;
+
+                for (int room = 0; room < roomsCount; room++)
+                {
+                    if (floor == floorsCount)
+                    {
+                        outputRow += $"L{floor}{room} ";
+                    }
+                    else if (floor % 2 == 0)
+                    {
+                        outputRow += $"O{floor}{room} ";
+                    }
+                    else if (floor % 2 != 0)
+                    {
+                        outputRow += $"A{floor}{room} ";
+                    }
+                }
+
+                Console.WriteLine(outputRow);
+            }
+        }
+    }
+}
---
+++ 06.NestedLoops_Lab/06.Building/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _06.Building
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int floors = int.Parse(Console.ReadLine());
+            int rooms = int.Parse(Console.ReadLine());
+
+            for (int floor = floors; floor >= 1; floor--)
+            {
+                List<string> roomsOnTheCurrentFloor = new();
+
+                for (int room = 0; room < rooms; room++)
+                {
+                    string roomName = string.Empty;
+
+                    if (floor == floors)
+                    {
+                        roomName = $"L{floor}{room}";
+                    }
+                    else if (floor % 2 != 0)
+                    {
+                        roomName = $"A{floor}{room}";
+                    }
+                    else if (floor % 2 == 0)
+                    {
+                        roomName = $"O{floor}{room}";
+                    }
+
+                    roomsOnTheCurrentFloor.Add(roomName);
+                }
+
+                Console.WriteLine(string.Join(" ", roomsOnTheCurrentFloor));
+            }
+        }
+    }
+}
