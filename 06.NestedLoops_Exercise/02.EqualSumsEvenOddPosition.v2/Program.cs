9de5ac718637fea79b8cf7b259a3800f33bca0ad
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-19 10:27:04 +0200

    Task 2 uploaded.
    Two solutions.

---
+++ 06.NestedLoops_Exercise/02.EqualSumsEvenOddPosition.v2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _02.EqualSumsEvenOddPosition.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = int.Parse(Console.ReadLine());
+            int x2 = int.Parse(Console.ReadLine());
+
+            List<int> validNumbers = new();
+
+            for (int number = x1; number <= x2; number++)
+            {
+                int oddSum = 0;
+                int evenSum = 0;
+                int numberCopy = number;
+
+                for (int position = 6; position >= 1; position--)
+                {
+                    int currentDigit = numberCopy % 10;
+
+                    if (position % 2 == 0)
+                    {
+                        evenSum += currentDigit;
+                    }
+                    else
+                    {
+                        oddSum += currentDigit;
+                    }
+
+                    numberCopy /= 10;
+                }
+
+                if (oddSum == evenSum)
+                {
+                    validNumbers.Add(number);
+                }
+            }
+
+            if (validNumbers.Any())
+            {
+                Console.WriteLine(string.Join(" ", validNumbers));
+            }
+        }
+    }
+}
---
+++ 06.NestedLoops_Exercise/02.EqualSumsEvenOddPosition/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _02.EqualSumsEvenOddPosition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            for (int currentNumber = firstNumber; currentNumber <= secondNumber; currentNumber++)
+            {
+                int tempNumber = currentNumber;
+                int oddSum = 0;
+                int evenSum = 0;
+                int position = 6;
+
+                while (tempNumber > 0)
+                {
+                    int digit = tempNumber % 10;
+
+                    if (position % 2 == 0)
+                    {
+                        evenSum += digit;
+                    }
+                    else
+                    {
+                        oddSum += digit;
+                    }
+
+                    position--;
+                    tempNumber /= 10;
+                }
+
+                if (oddSum == evenSum)
+                {
+                    Console.Write(currentNumber + " ");
+                }
+            }
+        }
+    }
+}
