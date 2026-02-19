9e6c045e4c19f46a380a82d3623ec530d9f2efeb
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-19 10:25:59 +0200

    Task 1 uploaded.
    Two solutions.

---
+++ 06.NestedLoops_Exercise/01.NumberPyramid.v2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _01.NumberPyramid.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            bool isBigger = false;
+            int currentNumber = 1;
+
+            for (int rows = 1; rows <= inputNumber; rows++)
+            {
+                for (int cols = 1; cols <= rows; cols++)
+                {
+                    Console.Write($"{currentNumber} ");
+                    currentNumber++;
+
+                    if (currentNumber > inputNumber)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                }
+
+                Console.WriteLine();
+
+                if (isBigger)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ 06.NestedLoops_Exercise/01.NumberPyramid/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.NumberPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            for (int rows = 1; counter < inputNumber; rows++)
+            {
+                for (int cols = 1; cols <= rows; cols++)
+                {
+                    counter++;
+                    Console.Write(counter + " ");
+
+                    if (counter == inputNumber)
+                    {
+                        break;
+                    }
+
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
