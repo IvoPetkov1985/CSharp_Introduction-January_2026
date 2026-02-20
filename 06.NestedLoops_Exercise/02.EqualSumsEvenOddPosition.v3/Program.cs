e1c2b1c86ca0712d76ba62ec07920e23d6cc463b
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-20 11:04:55 +0200

    Task 2 uploaded.
    Another two solutions.

---
+++ 06.NestedLoops_Exercise/02.EqualSumsEvenOddPosition.v3/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _02.EqualSumsEvenOddPosition.v3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = int.Parse(Console.ReadLine());
+            int endNumber = int.Parse(Console.ReadLine());
+
+            for (int currentNumber = startNumber; currentNumber <= endNumber; currentNumber++)
+            {
+                int evenSum = 0;
+                int oddSum = 0;
+
+                string numberAsString = currentNumber.ToString();
+
+                for (int index = 0; index < numberAsString.Length; index++)
+                {
+                    int currentDigit = int.Parse(numberAsString[index].ToString());
+
+                    if (index % 2 == 0)
+                    {
+                        evenSum += currentDigit;
+                    }
+                    else
+                    {
+                        oddSum += currentDigit;
+                    }
+                }
+
+                if (evenSum == oddSum)
+                {
+                    Console.Write(currentNumber + " ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 06.NestedLoops_Exercise/02.EqualSumsEvenOddPosition.v4/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _02.EqualSumsEvenOddPosition.v4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            List<int> validNumbers = new();
+
+            for (int currentNumber = firstNumber; currentNumber <= secondNumber; currentNumber++)
+            {
+                int firstDigit = currentNumber / 100000;
+                int secondDigit = currentNumber / 10000 % 10;
+                int thirdDigit = currentNumber / 1000 % 10;
+                int forthDigit = currentNumber / 100 % 10;
+                int fifthDigit = currentNumber / 10 % 10;
+                int sixthDigit = currentNumber % 10;
+
+                if (firstDigit + thirdDigit + fifthDigit == secondDigit + forthDigit + sixthDigit)
+                {
+                    validNumbers.Add(currentNumber);
+                }
+            }
+
+            if (validNumbers.Any())
+            {
+                Console.WriteLine(string.Join(" ", validNumbers));
+            }
+        }
+    }
+}
