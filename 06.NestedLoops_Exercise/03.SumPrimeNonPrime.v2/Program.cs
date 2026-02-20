86a65c8dd1731c0c40d5fa0b84b72defc3492f7a
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-20 12:07:36 +0200

    Tasks 3 and 5 uploaded.
    Each task with additional solution.

---
+++ 06.NestedLoops_Exercise/03.SumPrimeNonPrime.v2/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace _03.SumPrimeNonPrime.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            int sumPrime = 0;
+            int sumNonPrime = 0;
+
+            while (command != "stop")
+            {
+                int number = int.Parse(command);
+
+                if (number < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                    command = Console.ReadLine();
+                    continue;
+                }
+
+                if (IsPrime(number))
+                {
+                    sumPrime += number;
+                }
+                else
+                {
+                    sumNonPrime += number;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Sum of all prime numbers is: {sumPrime}");
+            Console.WriteLine($"Sum of all non prime numbers is: {sumNonPrime}");
+        }
+
+        static bool IsPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+
+            if (number == 2)
+            {
+                return true;
+            }
+
+            if (number % 2 == 0)
+            {
+                return false;
+            }
+
+            int divisor = 3;
+
+            while (divisor * divisor <= number)
+            {
+                if (number % divisor == 0)
+                {
+                    return false;
+                }
+
+                divisor++;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 06.NestedLoops_Exercise/05.SpecialNumbers.v3/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _05.SpecialNumbers.v3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            int start = 1111;
+            int end = 9999;
+
+            for (int current = start; current <= end; current++)
+            {
+                string currentAsString = current.ToString();
+                bool isSpecial = true;
+
+                for (int index = 0; index < currentAsString.Length; index++)
+                {
+                    int digit = int.Parse(currentAsString[index].ToString());
+
+                    if (digit == 0 || inputNumber % digit != 0)
+                    {
+                        isSpecial = false;
+                        break;
+                    }
+                }
+
+                if (isSpecial)
+                {
+                    Console.Write(current + " ");
+                }
+            }
+        }
+    }
+}
