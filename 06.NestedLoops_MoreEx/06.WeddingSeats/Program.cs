29e75173e1ed17ccb4af7fd3774aec1b45c26191
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-21 13:33:54 +0200

    Tasks 6 and 7 uploaded.

---
+++ 06.NestedLoops_MoreEx/06.WeddingSeats/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _06.WeddingSeats
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char lastSector = char.Parse(Console.ReadLine());
+            int rowsCount = int.Parse(Console.ReadLine());
+            int placesInOddRow = int.Parse(Console.ReadLine());
+
+            int placesInEvenRow = placesInOddRow + 2;
+            int counter = 0;
+
+            for (char sector = 'A'; sector <= lastSector; sector++)
+            {
+                for (int row = 1; row <= rowsCount; row++)
+                {
+                    if (row % 2 == 0)
+                    {
+                        for (char place = 'a'; place < 'a' + placesInEvenRow; place++)
+                        {
+                            Console.WriteLine($"{sector}{row}{place}");
+                            counter++;
+                        }
+                    }
+                    else
+                    {
+                        for (char place = 'a'; place < 'a' + placesInOddRow; place++)
+                        {
+                            Console.WriteLine($"{sector}{row}{place}");
+                            counter++;
+                        }
+                    }                    
+                }
+
+                rowsCount++;
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ 06.NestedLoops_MoreEx/07.SafePasswordsGenerator/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _07.SafePasswordsGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numA = int.Parse(Console.ReadLine());
+            int numB = int.Parse(Console.ReadLine());
+            int count = int.Parse(Console.ReadLine());
+
+            char symbolAStart = '#';
+            char symbolAEnd = '7';
+            char symbolBStart = '@';
+            char symbolBEnd = '`';
+
+            char currentA = symbolAStart;
+            char currentB = symbolBStart;
+            int counter = 0;
+
+            bool arePasswordsEnough = false;
+
+            for (int x = 1; x <= numA; x++)
+            {
+                for (int y = 1; y <= numB; y++)
+                {
+                    counter++;
+
+                    if (counter > count)
+                    {
+                        arePasswordsEnough = true;
+                        break;
+                    }
+
+                    Console.Write($"{currentA}{currentB}{x}{y}{currentB}{currentA}|");
+
+                    currentA++;
+                    currentB++;
+
+                    if (currentA > symbolAEnd)
+                    {
+                        currentA = symbolAStart;
+                    }
+
+                    if (currentB > symbolBEnd)
+                    {
+                        currentB = symbolBStart;
+                    }
+                }
+
+                if (arePasswordsEnough == true)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
