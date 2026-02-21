31334e5accac4ec2ff996ac6dea15845a78ee61f
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-21 13:35:25 +0200

    Task 10 uploaded.

---
+++ 06.NestedLoops_MoreEx/10.Profit/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _10.Profit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int oneLvCoins = int.Parse(Console.ReadLine());
+            int twoLvCoins = int.Parse(Console.ReadLine());
+            int fiveLvBanknotes = int.Parse(Console.ReadLine());
+            int sumToPay = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= oneLvCoins; i++)
+            {
+                for (int j = 0; j <= twoLvCoins; j++)
+                {
+                    for (int k = 0; k <= fiveLvBanknotes; k++)
+                    {
+                        if ((i * 1 + j * 2 + k * 5) == sumToPay)
+                        {
+                            Console.WriteLine($"{i} * 1 lv. + {j} * 2 lv. + {k} * 5 lv. = {sumToPay} lv.");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
