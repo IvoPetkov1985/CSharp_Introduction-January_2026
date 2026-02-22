40e1f0d0a6e75b5a2d04b1f2686e477cf4c39ac8
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-22 13:34:50 +0200

    Task 13 uploaded.
    Two solutions.

---
+++ 06.NestedLoops_MoreEx/13.PrimePairs.v2/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _13.PrimePairs.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int firstInterval = int.Parse(Console.ReadLine());
+            int secondInterval = int.Parse(Console.ReadLine());
+
+            for (int x1 = firstNumber; x1 <= firstNumber + firstInterval; x1++)
+            {
+                bool isX1Prime = true;
+
+                if (x1 % 2 == 0)
+                {
+                    isX1Prime = false;
+                }
+
+                for (int d = 3; d * d <= x1; d++)
+                {
+                    if (x1 % d == 0)
+                    {
+                        isX1Prime = false;
+                        break;
+                    }
+                }
+
+                if (isX1Prime)
+                {
+                    for (int x2 = secondNumber; x2 <= secondNumber + secondInterval; x2++)
+                    {
+                        bool isX2Prime = true;
+
+                        if (x2 % 2 == 0)
+                        {
+                            isX2Prime = false;
+                        }
+
+                        for (int d = 3; d * d <= x2; d++)
+                        {
+                            if (x2 % d == 0)
+                            {
+                                isX2Prime = false;
+                                break;
+                            }
+                        }
+
+                        if (isX2Prime)
+                        {
+                            Console.WriteLine($"{x1}{x2}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 06.NestedLoops_MoreEx/13.PrimePairs/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _13.PrimePairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int firstInterval = int.Parse(Console.ReadLine());
+            int secondInterval = int.Parse(Console.ReadLine());
+
+            for (int x1 = firstNum; x1 <= firstNum + firstInterval; x1++)
+            {
+                if (IsPrime(x1))
+                {
+                    for (int x2 = secondNum; x2 <= secondNum + secondInterval; x2++)
+                    {
+                        if (IsPrime(x2))
+                        {
+                            Console.WriteLine($"{x1}{x2}");
+                        }
+                    }
+                }
+            }
+        }
+
+        static bool IsPrime(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return false;
+            }
+
+            for (int d = 2; d < num; d++)
+            {
+                if (num % d == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
