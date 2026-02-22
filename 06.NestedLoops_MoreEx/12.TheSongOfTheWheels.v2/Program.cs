5c6c6e40c0ce89456ad5416655249f8c78ca3a91
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-22 13:34:08 +0200

    Task 12 uploaded.
    Two solutions.

---
+++ 06.NestedLoops_MoreEx/12.TheSongOfTheWheels.v2/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _12.TheSongOfTheWheels.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int controlValue = int.Parse(Console.ReadLine());
+            int passwordPosition = 4;
+
+            List<string> validCombinations = new();
+
+            for (int numA = 1; numA <= 9; numA++)
+            {
+                for (int numB = 1; numB <= 9; numB++)
+                {
+                    if (numA < numB)
+                    {
+                        for (int numC = 1; numC <= 9; numC++)
+                        {
+                            for (int numD = 1; numD <= 9; numD++)
+                            {
+                                if (numC > numD &&
+                                    (numA * numB + numC * numD) == controlValue)
+                                {
+                                    validCombinations.Add($"{numA}{numB}{numC}{numD}");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (validCombinations.Any())
+            {
+                Console.WriteLine(string.Join(" ", validCombinations));
+            }
+
+            if (validCombinations.Count >= passwordPosition)
+            {
+                string password = validCombinations[passwordPosition - 1];
+                Console.WriteLine($"Password: {password}");
+            }
+            else
+            {
+                Console.WriteLine("No!");
+            }
+        }
+    }
+}
---
+++ 06.NestedLoops_MoreEx/12.TheSongOfTheWheels/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _12.TheSongOfTheWheels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int controlValue = int.Parse(Console.ReadLine());
+
+            int combinationsCounter = 0;
+            string password = string.Empty;
+
+            for (int valueA = 1; valueA <= 9; valueA++)
+            {
+                for (int valueB = 1; valueB <= 9; valueB++)
+                {
+                    for (int valueC = 1; valueC <= 9; valueC++)
+                    {
+                        for (int valueD = 1; valueD <= 9; valueD++)
+                        {
+                            if ((valueA * valueB + valueC * valueD) == controlValue &&
+                                valueA < valueB &&
+                                valueC > valueD)
+                            {
+                                combinationsCounter++;
+                                Console.Write($"{valueA}{valueB}{valueC}{valueD} ");
+
+                                if (combinationsCounter == 4)
+                                {
+                                    password = $"{valueA}{valueB}{valueC}{valueD}";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (combinationsCounter > 0)
+            {
+                Console.WriteLine();
+            }
+
+            if (password != string.Empty)
+            {
+                Console.WriteLine($"Password: {password}");
+            }
+            else
+            {
+                Console.WriteLine("No!");
+            }
+        }
+    }
+}
