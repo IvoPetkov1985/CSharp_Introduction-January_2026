1fce60f59927a7e2efbb392b3a566c457f2859bd
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-23 09:30:16 +0200

    Tasks 1 and 2 solved.

---
+++ ExamPreparation_20250816/01.FruitMarket/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01.FruitMarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double strawberriesPrice = double.Parse(Console.ReadLine());
+            double bananasInKg = double.Parse(Console.ReadLine());
+            double orangesInKg = double.Parse(Console.ReadLine());
+            double raspberriesInKg = double.Parse(Console.ReadLine());
+            double strawberriesInKg = double.Parse(Console.ReadLine());
+
+            double raspberriesPrice = strawberriesPrice / 2;
+            double orangesPrice = raspberriesPrice * (1 - 0.40);
+            double bananasPrice = raspberriesPrice * (1 - 0.80);
+
+            double totalSum = strawberriesInKg * strawberriesPrice +
+                bananasInKg * bananasPrice +
+                orangesInKg * orangesPrice +
+                raspberriesInKg * raspberriesPrice;
+
+            Console.WriteLine($"{totalSum:F2}");
+        }
+    }
+}
---
+++ ExamPreparation_20250816/02.FamilyTrip/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _02.FamilyTrip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int nightsCount = int.Parse(Console.ReadLine());
+            double pricePerNight = double.Parse(Console.ReadLine());
+            int percentExpenses = int.Parse(Console.ReadLine());
+
+            double sumForNights = nightsCount * pricePerNight;
+
+            if (nightsCount > 7)
+            {
+                sumForNights *= 0.95;
+            }
+
+            double sumForExpenses = budget * percentExpenses / 100;
+            double totalSum = sumForNights + sumForExpenses;
+
+            if (budget >= totalSum)
+            {
+                Console.WriteLine($"Ivanovi will be left with {budget - totalSum:F2} leva after vacation.");
+            }
+            else
+            {
+                Console.WriteLine($"{totalSum - budget:F2} leva needed.");
+            }
+        }
+    }
+}
