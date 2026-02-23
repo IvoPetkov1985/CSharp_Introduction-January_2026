4fd060fa54c25e4a9575b1f93ed964b5310b0b72
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-23 09:31:46 +0200

    Tasks 5 and 6 solved.

---
+++ ExamPreparation_20250816/05.EasterEggsBattle/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _05.EasterEggsBattle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstPlayerEggs = int.Parse(Console.ReadLine());
+            int secondPlayerEggs = int.Parse(Console.ReadLine());
+
+            int firstEggs = firstPlayerEggs;
+            int secondEggs = secondPlayerEggs;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "End")
+                {
+                    Console.WriteLine($"Player one has {firstEggs} eggs left.");
+                    Console.WriteLine($"Player two has {secondEggs} eggs left.");
+                    break;
+                }
+
+                switch (command)
+                {
+                    case "one": secondEggs--; break;
+                    case "two": firstEggs--; break;
+                }
+
+                if (firstEggs == 0)
+                {
+                    Console.WriteLine($"Player one is out of eggs. Player two has {secondEggs} eggs left.");
+                    break;
+                }
+
+                if (secondEggs == 0)
+                {
+                    Console.WriteLine($"Player two is out of eggs. Player one has {firstEggs} eggs left.");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ ExamPreparation_20250816/06.BarcodeGenerator/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _06.BarcodeGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = int.Parse(Console.ReadLine());
+            int endNumber = int.Parse(Console.ReadLine());
+
+            List<int> validBarcodes = new();
+
+            int firstStartDigit = startNumber / 1000;
+            int secondStartDigit = startNumber / 100 % 10;
+            int thirdStartDigit = startNumber / 10 % 10;
+            int forthStartDigit = startNumber % 10;
+
+            int firstEndDigit = endNumber / 1000;
+            int secondEndDigit = endNumber / 100 % 10;
+            int thirdEndDigit = endNumber / 10 % 10;
+            int forthEndDigit = endNumber % 10;
+
+            for (int x1 = firstStartDigit; x1 <= firstEndDigit; x1++)
+            {
+                if (x1 % 2 != 0)
+                {
+                    for (int x2 = secondStartDigit; x2 <= secondEndDigit; x2++)
+                    {
+                        if (x2 % 2 != 0)
+                        {
+                            for (int x3 = thirdStartDigit; x3 <= thirdEndDigit; x3++)
+                            {
+                                if (x3 % 2 != 0)
+                                {
+                                    for (int x4 = forthStartDigit; x4 <= forthEndDigit; x4++)
+                                    {
+                                        if (x4 % 2 != 0)
+                                        {
+                                            validBarcodes.Add(int.Parse($"{x1}{x2}{x3}{x4}"));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", validBarcodes));
+        }
+    }
+}
