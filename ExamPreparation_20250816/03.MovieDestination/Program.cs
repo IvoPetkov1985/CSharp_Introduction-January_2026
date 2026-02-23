88bb64465d0cab9c4e137ae2509b55a20412fd1d
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-23 09:31:01 +0200

    Tasks 3 and 4 solved.

---
+++ ExamPreparation_20250816/03.MovieDestination/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace _03.MovieDestination
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string destination = Console.ReadLine();
+            string season = Console.ReadLine();
+            int daysCount = int.Parse(Console.ReadLine());
+
+            double pricePerDay = 0;
+
+            switch (season)
+            {
+                case "Winter":
+
+                    if (destination == "Dubai")
+                    {
+                        pricePerDay = 45000;
+                    }
+                    else if (destination == "Sofia")
+                    {
+                        pricePerDay = 17000;
+                    }
+                    else if (destination == "London")
+                    {
+                        pricePerDay = 24000;
+                    }
+
+                    break;
+
+                case "Summer":
+
+                    if (destination == "Dubai")
+                    {
+                        pricePerDay = 40000;
+                    }
+                    else if (destination == "Sofia")
+                    {
+                        pricePerDay = 12500;
+                    }
+                    else if (destination == "London")
+                    {
+                        pricePerDay = 20250;
+                    }
+
+                    break;
+            }
+
+            double finalPrice = daysCount * pricePerDay;
+
+            switch (destination)
+            {
+                case "Dubai":
+                    finalPrice *= 0.70;
+                    break;
+                case "Sofia":
+                    finalPrice *= 1.25;
+                    break;
+            }
+
+            if (budget >= finalPrice)
+            {
+                Console.WriteLine($"The budget for the movie is enough! We have {budget - finalPrice:F2} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"The director needs {finalPrice - budget:F2} leva more!");
+            }
+        }
+    }
+}
---
+++ ExamPreparation_20250816/04.FoodForPets/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _04.FoodForPets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            double totalFood = double.Parse(Console.ReadLine());
+
+            double bisquits = 0;
+            double dogEaten = 0;
+            double catEaten = 0;
+
+            for (int day = 1; day <= days; day++)
+            {
+                double dogFood = double.Parse(Console.ReadLine());
+                double catFood = double.Parse(Console.ReadLine());
+
+                if (day % 3 == 0)
+                {
+                    bisquits += (dogFood + catFood) * 0.1;
+                }
+
+                dogEaten += dogFood;
+                catEaten += catFood;
+            }
+
+            double totalEaten = dogEaten + catEaten;
+            double percentEatenFood = totalEaten * 100 / totalFood;
+            double percentDogEaten = dogEaten * 100 / totalEaten;
+            double percentCatEaten = catEaten * 100 / totalEaten;
+
+            Console.WriteLine($"Total eaten biscuits: {Math.Round(bisquits, 0)}gr.");
+            Console.WriteLine($"{percentEatenFood:F2}% of the food has been eaten.");
+            Console.WriteLine($"{percentDogEaten:F2}% eaten from the dog.");
+            Console.WriteLine($"{percentCatEaten:F2}% eaten from the cat.");
+        }
+    }
+}
