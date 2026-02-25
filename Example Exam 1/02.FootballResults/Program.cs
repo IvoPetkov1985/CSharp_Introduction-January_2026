046342fef19cee8557ad48e92ec13604a428d9a2
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-25 13:48:28 +0200

    Tasks 1 and 2 uploaded.

---
+++ Example Exam 1/02.FootballResults/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace _02.FootballResults
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstMatchResult = Console.ReadLine();
+            string secondMatchResult = Console.ReadLine();
+            string thirdMatchResult = Console.ReadLine();
+
+            int firstMatchHome = int.Parse(firstMatchResult[0].ToString());
+            int firstMatchGuest = int.Parse(firstMatchResult[2].ToString());
+
+            int secondMatchHome = int.Parse(secondMatchResult[0].ToString());
+            int secondMatchGuest = int.Parse(secondMatchResult[2].ToString());
+
+            int thirdMatchHome = int.Parse(thirdMatchResult[0].ToString());
+            int thirdMatchGuest = int.Parse(thirdMatchResult[2].ToString());
+
+            int won = 0;
+            int lost = 0;
+            int drawn = 0;
+
+            if (firstMatchHome > firstMatchGuest)
+            {
+                won++;
+            }
+            else if (firstMatchHome < firstMatchGuest)
+            {
+                lost++;
+            }
+            else
+            {
+                drawn++;
+            }
+
+            if (secondMatchHome > secondMatchGuest)
+            {
+                won++;
+            }
+            else if (secondMatchHome < secondMatchGuest)
+            {
+                lost++;
+            }
+            else
+            {
+                drawn++;
+            }
+
+            if (thirdMatchHome > thirdMatchGuest)
+            {
+                won++;
+            }
+            else if (thirdMatchHome < thirdMatchGuest)
+            {
+                lost++;
+            }
+            else
+            {
+                drawn++;
+            }
+
+            Console.WriteLine($"Team won {won} games.");
+            Console.WriteLine($"Team lost {lost} games.");
+            Console.WriteLine($"Drawn games: {drawn}");
+        }
+    }
+}
