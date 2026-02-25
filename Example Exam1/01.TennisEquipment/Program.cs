e988a5899c678ebef083dc4602316037d176616f
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-25 13:46:50 +0200

    Tasks 1 and 2 uploaded.

---
+++ Example Exam1/01.TennisEquipment/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.TennisEquipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double tennisRacketPrice = double.Parse(Console.ReadLine());
+            int tennisRacketsCount = int.Parse(Console.ReadLine());
+            int pairsSneakersCount = int.Parse(Console.ReadLine());
+
+            double pairSneakersPrice = tennisRacketPrice / 6;
+            double sumRacketsSneakers = tennisRacketPrice * tennisRacketsCount + pairsSneakersCount * pairSneakersPrice;
+            double otherEquipmentPrice = sumRacketsSneakers * 0.2;
+            double totalSum = sumRacketsSneakers + otherEquipmentPrice;
+            double playerSum = totalSum / 8;
+            double sponsorSum = totalSum - playerSum;
+
+            Console.WriteLine($"Price to be paid by Djokovic {Math.Floor(playerSum)}");
+            Console.WriteLine($"Price to be paid by sponsors {Math.Ceiling(sponsorSum)}");
+        }
+    }
+}
