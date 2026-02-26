6a7a1e09efc8b8b469535ff0f240c87aa5e5f1fe
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-26 15:04:44 +0200

    Tasks 1 and 2 uploaded.

---
+++ Example Exam 2/01.OscarsCeremony/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.OscarsCeremony
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hallRent = int.Parse(Console.ReadLine());
+
+            double statues = hallRent - hallRent * 0.3;
+            double catering = statues - statues * 0.15;
+            double sound = catering * 0.5;
+
+            double totalExpenses = hallRent + statues + catering + sound;
+            Console.WriteLine($"{totalExpenses:F2}");
+        }
+    }
+}
---
+++ Example Exam 2/02.GodzillaVsKong/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _02.GodzillaVsKong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int extras = int.Parse(Console.ReadLine());
+            double clothingPricePerExtra = double.Parse(Console.ReadLine());
+
+            double decorPrice = budget * 0.1;
+            double clothingPrice = clothingPricePerExtra * extras;
+
+            if (extras > 150)
+            {
+                clothingPrice *= 0.9;
+            }
+
+            double finalSum = clothingPrice + decorPrice;
+
+            if (budget < finalSum)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {finalSum - budget:F2} leva more.");
+            }
+            else
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {budget - finalSum:F2} leva left.");
+            }
+        }
+    }
+}
