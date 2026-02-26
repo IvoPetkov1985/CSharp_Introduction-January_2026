5421b11ca9a2094e4733445577fc5eae0ee96e2b
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-02-26 15:06:41 +0200

    Task 5 uploaded.
    Two fancy solutions for task 5.

---
+++ Example Exam 2/05.MovieRatings.v2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _05.MovieRatings.v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            double[] ratings = new double[count];
+            string[] movies = new string[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                string movie = Console.ReadLine();
+                double rating = double.Parse(Console.ReadLine());
+                movies[i] = movie;
+                ratings[i] = rating;
+            }
+
+            double maxRating = ratings.Max();
+            string maxRatedMovie = movies[Array.IndexOf(ratings, maxRating)];
+            double minRating = ratings.Min();
+            string minRatedMovie = movies[Array.IndexOf(ratings, minRating)];
+            double averageRating = ratings.Average();
+
+            Console.WriteLine($"{maxRatedMovie} is with highest rating: {maxRating:F1}");
+            Console.WriteLine($"{minRatedMovie} is with lowest rating: {minRating:F1}");
+            Console.WriteLine($"Average rating: {averageRating:F1}");
+        }
+    }
+}
---
+++ Example Exam 2/05.MovieRatings/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _05.MovieRatings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int movies = int.Parse(Console.ReadLine());
+            Dictionary<string, double> moviesRatings = new();
+
+            for (int movie = 0; movie < movies; movie++)
+            {
+                string name = Console.ReadLine();
+                double rating = double.Parse(Console.ReadLine());
+                moviesRatings.Add(name, rating);
+            }
+
+            KeyValuePair<string, double> highestRatingMovie = moviesRatings.OrderByDescending(x => x.Value).First();
+            KeyValuePair<string, double> lowestRatingMovie = moviesRatings.OrderBy(x => x.Value).First();
+            double average = moviesRatings.Values.Average();
+
+            Console.WriteLine($"{highestRatingMovie.Key} is with highest rating: {highestRatingMovie.Value:F1}");
+            Console.WriteLine($"{lowestRatingMovie.Key} is with lowest rating: {lowestRatingMovie.Value:F1}");
+            Console.WriteLine($"Average rating: {average:F1}");
+        }
+    }
+}
