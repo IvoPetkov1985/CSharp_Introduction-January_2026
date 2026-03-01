50c65cc7588896a1463bcd7052e01232a908b4b8
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-01 18:48:47 +0200

    Tasks 3 and 4 uploaded.

---
+++ RegularExam20260228/03.FinalCompetition/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _03.FinalCompetition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dancers = int.Parse(Console.ReadLine());
+            double points = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string destination = Console.ReadLine();
+
+            double sum = dancers * points;
+
+            if (destination == "Abroad")
+            {
+                sum *= 1.5;
+            }
+
+            int expensesPercent = 0;
+
+            if (destination == "Bulgaria")
+            {
+                switch (season)
+                {
+                    case "summer": expensesPercent = 5; break;
+                    case "winter": expensesPercent = 8; break;
+                }
+            }
+            else if (destination == "Abroad")
+            {
+                switch (season)
+                {
+                    case "summer": expensesPercent = 10; break;
+                    case "winter": expensesPercent = 15; break;
+                }
+            }
+
+            double expenses = sum * expensesPercent / 100;
+            double nettoSum = sum - expenses;
+            double charitySum = nettoSum * 0.75;
+            double sumPerDancer = (nettoSum - charitySum) / dancers;
+
+            Console.WriteLine($"Charity - {charitySum:F2}");
+            Console.WriteLine($"Money per dancer - {sumPerDancer:F2}");
+        }
+    }
+}
---
+++ RegularExam20260228/04.GrandpaStavri/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _04.GrandpaStavri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+
+            double totalLiters = 0;
+            double totalDegrees = 0;
+
+            for (int day = 0; day < days; day++)
+            {
+                double liters = double.Parse(Console.ReadLine());
+                double degrees = double.Parse(Console.ReadLine());
+
+                totalLiters += liters;
+                totalDegrees += degrees * liters;
+            }
+
+            double averageDegrees = totalDegrees / totalLiters;
+
+            Console.WriteLine($"Liter: {totalLiters:F2}");
+            Console.WriteLine($"Degrees: {averageDegrees:F2}");
+
+            string output = string.Empty;
+
+            if (averageDegrees < 38)
+            {
+                output = "Not good, you should baking!";
+            }
+            else if (averageDegrees <= 42)
+            {
+                output = "Super!";
+            }
+            else
+            {
+                output = "Dilution with distilled water!";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
