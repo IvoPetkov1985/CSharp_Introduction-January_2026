22796c24a3a9b23074e36332175710159ab147e7
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-01 18:47:20 +0200

    Tasks 1 and 2 uploaded.

---
+++ RegularExam20260228/01.CatDiet/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _01.CatDiet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fatsPercent = int.Parse(Console.ReadLine());
+            int proteinsPercent = int.Parse(Console.ReadLine());
+            int carbohydratesPercent = int.Parse(Console.ReadLine());
+            int totalCalories = int.Parse(Console.ReadLine());
+            int waterPercent = int.Parse(Console.ReadLine());
+
+            double fatsInGrams = fatsPercent * totalCalories / 100d / 9;
+            double proteinsInGrams = proteinsPercent * totalCalories / 100d / 4;
+            double carbohydratesInGrams = carbohydratesPercent * totalCalories / 100d / 4;
+            double foodInGrams = fatsInGrams + proteinsInGrams + carbohydratesInGrams;
+            double caloriesPerGramFood = totalCalories / foodInGrams;
+            double caloriesInWater = waterPercent * caloriesPerGramFood / 100d;
+            double caloriesInGramDryFood = caloriesPerGramFood - caloriesInWater;
+
+            Console.WriteLine($"{caloriesInGramDryFood:F4}");
+        }
+    }
+}
---
+++ RegularExam20260228/02.BraceletStand/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _02.BraceletStand
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double moneyPerDay = double.Parse(Console.ReadLine());
+            double moneySalesPerDay = double.Parse(Console.ReadLine());
+            double expensesForThePeriod = double.Parse(Console.ReadLine());
+            double presentPrice = double.Parse(Console.ReadLine());
+            int daysCount = 5;
+
+            double totalSavedMoney = moneyPerDay * daysCount;
+            double totalProfitMoney = moneySalesPerDay * daysCount;
+            double allSavedMoney = totalSavedMoney + totalProfitMoney - expensesForThePeriod;
+
+            if (allSavedMoney >= presentPrice)
+            {
+                Console.WriteLine($"Profit: {allSavedMoney:F2} BGN, the gift has been purchased.");
+            }
+            else
+            {
+                Console.WriteLine($"Insufficient money: {presentPrice - allSavedMoney:F2} BGN.");
+            }
+        }
+    }
+}
