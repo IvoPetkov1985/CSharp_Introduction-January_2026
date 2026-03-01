a5afdba3fb2aa1882440a64b2a7321bebcff7cb2
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-01 18:49:39 +0200

    Tasks 5 and 6 uploaded.

---
+++ RegularExam20260228/05.BestPlayer/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _05.BestPlayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bestPlayerName = string.Empty;
+            int bestPlayerScore = 0;
+
+            while (true)
+            {
+                string playerName = Console.ReadLine();
+
+                if (playerName == "END")
+                {
+                    break;
+                }
+
+                int goalsScored = int.Parse(Console.ReadLine());
+
+                if (goalsScored > bestPlayerScore)
+                {
+                    bestPlayerName = playerName;
+                    bestPlayerScore = goalsScored;
+                }
+
+                if (goalsScored >= 10)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"{bestPlayerName} is the best player!");
+            bool hattrick = bestPlayerScore >= 3;
+
+            if (hattrick)
+            {
+                Console.WriteLine($"He has scored {bestPlayerScore} goals and made a hat-trick !!!");
+            }
+            else
+            {
+                Console.WriteLine($"He has scored {bestPlayerScore} goals.");
+            }
+        }
+    }
+}
---
+++ RegularExam20260228/06.EasterDecoration/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _06.EasterDecoration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int clients = int.Parse(Console.ReadLine());
+
+            double basketPrice = 1.50;
+            double wreathPrice = 3.80;
+            double bunnyPrice = 7.00;
+
+            double totalBill = 0;
+
+            for (int client = 0; client < clients; client++)
+            {
+                double clientBill = 0;
+                int productCounter = 0;
+
+                string purchase = Console.ReadLine();
+
+                while (purchase != "Finish")
+                {
+                    productCounter++;
+
+                    switch (purchase)
+                    {
+                        case "basket": clientBill += basketPrice; break;
+                        case "wreath": clientBill += wreathPrice; break;
+                        case "chocolate bunny": clientBill += bunnyPrice; break;
+                    }
+
+                    purchase = Console.ReadLine();
+                }
+
+                if (productCounter % 2 == 0)
+                {
+                    clientBill -= clientBill * 0.20;
+                }
+
+                Console.WriteLine($"You purchased {productCounter} items for {clientBill:F2} leva.");
+                totalBill += clientBill;
+            }
+
+            double averageBill = totalBill / clients;
+            Console.WriteLine($"Average bill per client is: {averageBill:F2} leva.");
+        }
+    }
+}
