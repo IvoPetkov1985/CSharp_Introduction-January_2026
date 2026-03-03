481acc81968ee4d848c0e767294cbd0e6a9aa2df
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-03 10:52:33 +0200

    Tasks 1 and 2 uploaded.

---
+++ Example Exam 5/01.PoolDay/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01.PoolDay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int visitorsCount = int.Parse(Console.ReadLine());
+            double entryTax = double.Parse(Console.ReadLine());
+            double sunbedTax = double.Parse(Console.ReadLine());
+            double umbrellaTax = double.Parse(Console.ReadLine());
+
+            double sunbedsNeeded = Math.Ceiling(visitorsCount * 0.75);
+            double umbrellasNeeded = Math.Ceiling(visitorsCount / 2d);
+
+            double sumEntryTaxes = visitorsCount * entryTax;
+            double sumSunbeds = sunbedsNeeded * sunbedTax;
+            double sumUmbrellas = umbrellasNeeded * umbrellaTax;
+            double finalSum = sumEntryTaxes + sumSunbeds + sumUmbrellas;
+
+            Console.WriteLine($"{finalSum:F2} lv.");
+        }
+    }
+}
---
+++ Example Exam 5/02.FamilyTrip/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _02.FamilyTrip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double familyBudget = double.Parse(Console.ReadLine());
+            int nightsCount = int.Parse(Console.ReadLine());
+            double pricePerNight = double.Parse(Console.ReadLine());
+            int additionalExpensesPercent = int.Parse(Console.ReadLine());
+            int discountPercent = 5;
+
+            if (nightsCount > 7)
+            {
+                pricePerNight -= pricePerNight * discountPercent / 100d;
+            }
+
+            double additionalExpensesPrice = familyBudget * additionalExpensesPercent / 100d;
+            double sumNights = pricePerNight * nightsCount;
+            double tripPrice = sumNights + additionalExpensesPrice;
+
+            if (familyBudget >= tripPrice)
+            {
+                Console.WriteLine($"Ivanovi will be left with {familyBudget - tripPrice:F2} leva after vacation.");
+            }
+            else
+            {
+                Console.WriteLine($"{tripPrice - familyBudget:F2} leva needed.");
+            }
+        }
+    }
+}
