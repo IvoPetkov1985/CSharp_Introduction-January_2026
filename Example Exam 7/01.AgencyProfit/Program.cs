9588db75e3be1b7fb730e21f4111622c08e775eb
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2026-03-05 14:06:44 +0200

    Tasks 1 and 2 uploaded.

---
+++ Example Exam 7/01.AgencyProfit/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.AgencyProfit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string companyName = Console.ReadLine();
+            int adultTickets = int.Parse(Console.ReadLine());
+            int kidTickets = int.Parse(Console.ReadLine());
+            double adultNettoPrice = double.Parse(Console.ReadLine());
+            double servicePrice = double.Parse(Console.ReadLine());
+
+            double kidNettoPrice = adultNettoPrice * 0.3;
+            double adultTotalPrice = adultNettoPrice + servicePrice;
+            double kidTotalPrice = kidNettoPrice + servicePrice;
+            double totalCompanySum = adultTotalPrice * adultTickets + kidTotalPrice * kidTickets;
+            double profit = totalCompanySum * 0.2;
+
+            Console.WriteLine($"The profit of your agency from {companyName} tickets is {profit:F2} lv.");
+        }
+    }
+}
---
+++ Example Exam 7/02.AddBags/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _02.AddBags
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double luggageTax = double.Parse(Console.ReadLine());
+            double kilosLuggage = double.Parse(Console.ReadLine());
+            int daysCount = int.Parse(Console.ReadLine());
+            int luggagesCount = int.Parse(Console.ReadLine());
+
+            if (kilosLuggage < 10)
+            {
+                luggageTax *= 0.2;
+            }
+            else if (kilosLuggage <= 20)
+            {
+                luggageTax *= 0.5;
+            }
+
+            if (daysCount < 7)
+            {
+                luggageTax *= 1.40;
+            }
+            else if (daysCount <= 30)
+            {
+                luggageTax *= 1.15;
+            }
+            else
+            {
+                luggageTax *= 1.10;
+            }
+
+            double bagsPrice = luggageTax * luggagesCount;
+            Console.WriteLine($"The total price of bags is: {bagsPrice:F2} lv.");
+        }
+    }
+}
